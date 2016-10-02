9129f651efb233bb0daa7f85e72c5542b9e15052
Pär Thernström <par@earthpeople.se>
2016-10-02 23:27:31 +0200

    add feeds package

---
+++ app/Http/Controllers/FeedController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FeedController
+{
+    /*
+    Feeds use https://github.com/willvincent/feeds
+    */
+    public function update()
+    {
+
+        $RssURL = 'https://polisen.se/Stockholms_lan/Aktuellt/Handelser/Handelser-i-hela-landet/?feed=rss';
+
+        return [123];
+
+    }
+
+}
