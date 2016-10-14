451e2d19cf54a39536523d5c1c62e728a794ab95
Pär Thernström <par@earthpeople.se>
2016-10-14 20:45:41 +0200

    styles

--- resources/views/single-event.blade.php
+++ resources/views/single-event.blade.php
@@ -11,40 +11,6 @@ Template for start page
 
 @section('content')
 
-    <article class="Event Event--single">
-
-        <h1 class="Event__title">
-            {{ $event->parsed_title }}
-        </h1>
-        <!--
-        Om bara vill visa när skillnad är mer än nn dagar/timmar osv.
-        http://stackoverflow.com/questions/23336261/laravel-carbon-display-date-difference-only-in-days
-        -->
-        <p>{{ $event->getPubDateFormattedForHumans() }} - {{ $event->getPubDateFormatted() }}</p>
-
-        <p>
-            @if ($event->parsed_title_location)
-                 {{ $event->parsed_title_location }},
-            @endif
-
-            @if ($event->administrative_area_level_2)
-                {{ $event->administrative_area_level_2 }},
-            @endif
-
-            @if ($event->administrative_area_level_1)
-                {{ $event->administrative_area_level_1 }},
-            @endif
-        </p>
-
-        @if ($event->geocoded)
-            <p class="Event__map">
-                <amp-img class="Event__mapImage" src="{{ $event->getStaticImageSrc(640,320) }}" width="640" height="320" layout="responsive"></amp-img>
-            </p>
-        @endif
-
-        <p class="Event__teaser">{!! nl2br($event->description) !!}</p>
-        <p class="Event__content">{!! nl2br($event->parsed_content) !!}</p>
-
-    </article>
+    @include('parts.crimeevent', ["single" => true])
 
 @endsection
--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -15,36 +15,7 @@ Template for start page
 
         @foreach ($events as $event)
 
-        <article class="Event Event--overview">
-
-            @if ($event->geocoded)
-                <p class="Event__map">
-                    <a href="{{ $event->getPermalink() }}">
-                        <amp-img class="Event__mapImage" src="{{ $event->getStaticImageSrc(640,320) }}" width="640" height="320" layout="responsive"></amp-img>
-                    </a>
-                </p>
-            @endif
-
-            <h1 class="Event__title">
-                <a href="{{ $event->getPermalink() }}">
-                    {{ $event->parsed_title }}
-                </a>
-            </h1>
-            <!--
-            Om bara vill visa när skillnad är mer än nn dagar/timmar osv.
-            http://stackoverflow.com/questions/23336261/laravel-carbon-display-date-difference-only-in-days
-            -->
-            <p class="Event__meta">
-                <span class="Event__location">{{ $event->parsed_title_location }}</span>
-                <span class="Event__metaDivider">|</span>
-                <span class="Event__dateHuman">{{ $event->getPubDateFormattedForHumans() }}</span>
-                <!-- <span class="Event__dateFormatted">{{ $event->getPubDateFormatted() }}</span> -->
-            </p>
-
-            <p class="Event__teaser">{!! nl2br($event->description) !!}</p>
-            <p class="Event__content">{!! nl2br($event->parsed_content) !!}</p>
-
-        </article>
+            @include('parts.crimeevent', ["overview" => true])
 
         @endforeach
 
