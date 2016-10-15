6837841511369884919ba9ba1b45026f4d8638c1
Pär Thernström <par@earthpeople.se>
2016-10-15 15:55:08 +0200

    meta description and image

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -21,6 +21,10 @@ Layout template for web
     <meta name="description" content="@yield('metaDescription')">
     @endif
 
+    @hasSection('metaImage')
+    <meta property="og:image" content="@yield('metaImage')" />
+    @endif
+
     <title>@yield('title') → Brottsplatskartan</title>
 
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
