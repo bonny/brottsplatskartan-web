d77b081d46519a28c6244da7fcdc19174ffa7207
Pär Thernström <par@earthpeople.se>
2016-10-16 20:01:27 +0200

    columns

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -28,10 +28,7 @@ Layout template for web
     <title>@yield('title') → Brottsplatskartan</title>
 
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-    <style amp-custom>
-        {{--@include('css-styles') --}}
-        {{ include(public_path().'/css/styles.css') }}
-    </style>
+    <style amp-custom>@include('css-styles')</style>
 
 </head>
 <body>
