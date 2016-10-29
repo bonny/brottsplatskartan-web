57d8f1a7de377c2d6b03893f523e987ceba645bd
Pär Thernström <par@earthpeople.se>
2016-10-29 15:17:14 +0200

    move css to own real css file

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -58,7 +58,8 @@ Layout template for web
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-    <style amp-custom>@include('css-styles')</style>
+    <style amp-custom>{!! file_get_contents( public_path("css/styles.css") ) !!}</style>
+
 
 </head>
 <body>
