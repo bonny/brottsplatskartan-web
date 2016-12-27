e7eb837295c653ae84447886919611d338cef4b7
Pär Thernström <par.thernstrom@gmail.com>
2016-12-27 12:09:37 +0100

    remove api key

---
+++ app/CrimeEvent.php
@@ -0,0 +1,585 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+use App\Http\Controllers\FeedParserController;
+use App\Http\Controllers\FeedController;
+
+class CrimeEvent extends Model
+{
+
+    protected $fillable = [
+        'title',
+        'description',
+        'permalink',
+        'pubdate',
+        'pubdate_iso8601',
+        'md5',
+        'parsed_date',
+        'parsed_title',
+        'parsed_title_location',
+        'parsed_content_location',
+        'parsed_content',
+        'location_lng',
+        'location_lat',
+        'parsed_teaser',
+        'parsed_content',
+        'scanned_for_locations',
+        'geocoded'
+    ];
+
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function locations()
+    {
+        return $this->hasMany('App\Locations');
+    }
+
+    // return src for an image
+    public function getStaticImageSrc($width = 320, $height = 320, $scale = 1) {
+
+        $google_api_key = env("GOOGLE_API_KEY");
+
+        $image_src = "https://maps.googleapis.com/maps/api/staticmap?";
+        $image_src .= "key=$google_api_key";
+        $image_src .= "&size={$width}x{$height}";
+        $image_src .= "&scale={$scale}";
+        $image_src .= "&language=sv";
+
+        // if viewport info exists use that and skip manual zoom level
+        if ($this->viewport_northeast_lat) {
+
+            $image_src .= "&path=";
+            $image_src .= "color:0x00000000|weight:2|fillcolor:0xFF660044";
+
+            /*
+
+            color:
+            (optional) specifies a color either as a
+            24-bit (example: color=0xFFFFCC) or
+            32-bit hexadecimal value (example: color=0xFFFFCCFF), or from the set {black, brown, green, purple, yellow, blue, gray, orange, red, white}.
+
+            example from google api:
+            path=color:0xFFFFCC|weight:5|fillcolor:0xFFFF0033|8th+Avenue+%26+34th+St,New+York,NY|\
+            8th+Avenue+%26+42nd+St,New+York,NY|Park+Ave+%26+42nd+St,New+York,NY,NY|\
+            Park+Ave+%26+34th+St,New+York,NY,NY
+
+            */
+
+            $image_src .= "|{$this->viewport_northeast_lat},{$this->viewport_northeast_lng}";
+            $image_src .= "|{$this->viewport_southwest_lat},{$this->viewport_northeast_lng}";
+
+            $image_src .= "|{$this->viewport_southwest_lat},{$this->viewport_southwest_lng}";
+            $image_src .= "|{$this->viewport_northeast_lat},{$this->viewport_southwest_lng}";
+
+        } else if ($this->location_lat) {
+
+            // no viewport but location_lat, fallback to center
+            $image_src .= "&center={$this->location_lat},{$this->location_lng}";
+            $image_src .= "&zoom=14";
+        } else {
+
+            // @TODO: return fallback iamge
+            return "";
+
+        }
+
+        #echo "image: <img src='$image_src'>";
+        #exit;
+
+        // src="https://maps.googleapis.com/maps/api/staticmap?center={{ $event->location_lat }},{{ $event->location_lng }}&zoom=14&size=600x400&key=...&markers={{ $event->location_lat }},{{ $event->location_lng }}"
+        return $image_src;
+
+    }
+
+    // get image far away, like whole sweden or something
+    public function getStaticImageSrcFar($width = 320, $height = 320, $scale = 1) {
+
+        $google_api_key = env("GOOGLE_API_KEY");
+
+        $image_src = "https://maps.googleapis.com/maps/api/staticmap?";
+        $image_src .= "key=$google_api_key";
+        $image_src .= "&size={$width}x{$height}";
+        $image_src .= "&scale={$scale}";
+        $image_src .= "&language=sv";
+
+        // if viewport info exists use that and skip manual zoom level
+        if ($this->location_lat) {
+
+            // no viewport but location_lat, fallback to center
+            $image_src .= "&center={$this->location_lat},{$this->location_lng}";
+            $image_src .= "&zoom=5";
+
+            $image_src .= "&markers=color:red|{$this->location_lat},{$this->location_lng}";
+
+        } else {
+
+            return "";
+
+        }
+
+        return $image_src;
+
+    }
+
+    /**
+     * The pub date is the date from the RSS-feed,
+     * i.e. when the crime is posted by polisen
+     * the actual event may have happened much earlier
+     */
+    public function getPubDateISO8601() {
+
+        return Carbon::createFromTimestamp($this->pubdate)->toIso8601String();
+
+    }
+
+    public function getPubDateFormatted($format = '%A %d %B %Y') {
+
+        return Carbon::createFromTimestamp($this->pubdate)->formatLocalized($format);
+
+    }
+
+    public function getPubDateFormattedForHumans() {
+
+        return Carbon::createFromTimestamp($this->pubdate)->diffForHumans();
+
+    }
+
+    /**
+     * Parsed date = the date that is written as text in each crime
+     * Is often much earlier that the RSS data
+     */
+    function getParsedDateFormattedForHumans() {
+
+        return Carbon::createFromTimestamp(strtotime($this->parsed_date))->diffForHumans();
+
+    }
+
+    public function getParsedDateISO8601() {
+
+        return Carbon::createFromTimestamp(strtotime($this->parsed_date))->toIso8601String();
+
+    }
+
+
+    /**
+     * Returns a nice permalink to the page
+     *
+     * Example return value:
+     * /varmlands-lan/rattfylleri-2440
+     */
+    public function getPermalink($absolute = false) {
+
+        $slugParts = [];
+
+        if ( ! empty($this->administrative_area_level_1) ) {
+            $lan = $this->toAscii($this->administrative_area_level_1);
+        } else {
+            $lan = "sverige";
+        }
+
+        // "Stöld/inbrott" and so on
+        $slugParts[] = $this->parsed_title;
+
+        if ( ! empty($this->parsed_title_location) ) {
+            $slugParts[] = $this->parsed_title_location;
+        } else {
+            #$eventName = "";
+        }
+
+        $prio1locations = $this->locations->filter(function($val, $key) {
+            return $val->prio == 1;
+        });
+
+        foreach ($prio1locations as $location) {
+            $slugParts[] = $location->name;
+        }
+
+        $slugParts[] = $this->getKey();
+
+        $eventName = implode("-", $slugParts);
+        $eventName = $this->toAscii($eventName);
+
+        $permalink = route("singleEvent", [
+            "lan" => $lan,
+            "eventName" => $eventName
+        ], $absolute);
+
+        return $permalink;
+
+    }
+
+    /**
+     * Hämta eventets platser i en rimligt fint formaterad sträng
+     * typ såhär: Borås, nnn län
+     */
+    public function getLocationString($includePrioLocations = true, $includeParsedTitleLocation = true, $inclueAdministrativeAreaLevel1Locations = true) {
+
+        $locations = [];
+
+        if ($includePrioLocations) {
+
+            $prioLocations = $this->locations->whereIn("prio", [1, 2]);
+
+            if ($prioLocations->count()) {
+                foreach ($prioLocations as $oneLocation) {
+                    $locations[] = title_case($oneLocation->name);
+                }
+            }
+
+        }
+
+        if ($includeParsedTitleLocation) {
+            if ($this->parsed_title_location) {
+                $locations[] = $this->parsed_title_location;
+            }
+        }
+
+        if ($inclueAdministrativeAreaLevel1Locations) {
+            if ($this->administrative_area_level_1 && $this->administrative_area_level_1 !== $this->parsed_title_location) {
+                $locations[] = $this->administrative_area_level_1;
+            }
+        }
+
+        $location = implode(", ", $locations);
+
+        return $location;
+
+    }
+
+    /**
+     * Som getLocationString
+     * men platser kan vara länkade, t.ex. länen
+     */
+    public function getLocationStringWithLinks() {
+
+        $locations = [];
+
+        $prioOneLocations = $this->locations->whereIn("prio", [1, 2]);
+
+        if ($prioOneLocations->count()) {
+            foreach ($prioOneLocations as $oneLocation) {
+
+                $locations[] = sprintf(
+                    '<a href="%2$s">%1$s</a>',
+                    title_case($oneLocation->name),
+                    route("platsSingle", ["plats" => $oneLocation->name])
+                );
+
+            }
+        }
+
+        if ($this->parsed_title_location) {
+
+            //$locations[] = $this->parsed_title_location;
+            $locations[] = sprintf(
+                '<a href="%2$s">%1$s</a>',
+                $this->parsed_title_location,
+                route("platsSingle", ["plats" => $this->parsed_title_location])
+            );
+
+        }
+
+        if ($this->administrative_area_level_1 && $this->administrative_area_level_1 !== $this->parsed_title_location) {
+
+            $locations[] = sprintf(
+                '<a href="%2$s">%1$s</a>',
+                $this->administrative_area_level_1,
+                route("lanSingle", ["lan" => $this->administrative_area_level_1])
+            );
+
+        }
+
+        $location = implode(", ", $locations);
+
+        return $location;
+
+    }
+
+    // from http://cubiq.org/the-perfect-php-clean-url-generator
+    // @TODO: put in global helper
+    public function toAscii($str, $replace=array(), $delimiter='-') {
+
+    	if( !empty($replace) ) {
+    		$str = str_replace((array)$replace, ' ', $str);
+    	}
+
+    	$clean = iconv('UTF-8', 'ASCII//TRANSLIT', $str);
+    	$clean = preg_replace("![^a-zA-Z0-9/_|+ -]!", '', $clean);
+    	$clean = strtolower(trim($clean, '-'));
+    	$clean = preg_replace("![/_|+ -]+!", $delimiter, $clean);
+
+    	return $clean;
+    }
+
+    /**
+     * @TODO: shorten!
+     * @return string
+     */
+    public function getMetaDescription($length = 155) {
+
+        $text = "";
+
+        $text .= trim($this->description) . " " . trim($this->parsed_content);
+
+        $text = strip_tags(str_replace('<', ' <', $text));
+        $text = str_limit($text, $length);
+
+        return $text;
+
+    }
+
+    // https://laracasts.com/discuss/channels/laravel/search-option-in-laravel-5?page=1
+    /*
+    public static function scopeSearchByKeyword($query, $keyword) {
+
+        if ($keyword != '') {
+
+            $query->where(function ($query) use ($keyword) {
+
+                $query->where("description", "LIKE","%$keyword%")
+                    ->orWhere("parsed_title_location", "LIKE", "%$keyword%")
+                    ->orWhere("parsed_content", "LIKE", "%$keyword%")
+                    ->orWhere("parsed_title", "LIKE", "%$keyword%");
+
+            });
+        }
+
+        return $query;
+    }
+    */
+
+    /**
+     * Get the description (kinda the teaser)
+     * replacing new lines with <p>
+     */
+    public function getDescription() {
+
+        $text = $this->description;
+
+        $text = $this->autop($text);
+
+        return $text;
+
+    }
+
+    /**
+     * Get the description
+     */
+    public function getParsedContent() {
+
+        $text = $this->parsed_content;
+
+        $text = $this->autop($text);
+
+        return $text;
+
+    }
+
+    /**
+     * Get the description, for overview pages, where text is cropped after nn chars
+     * and styles removed to not interfere in listing
+     */
+    public function getParsedContentTeaser($length = 160) {
+
+        $text = $this->parsed_content;
+
+        // strip tags but make sure there is at least a space where the tag was
+        // so text paragraphs don't collapse
+        $text = strip_tags(str_replace('<', ' <', $text));
+
+        $text = str_limit($text, $length);
+
+        // $text = $this->autop($text);
+
+        return $text;
+
+    }
+
+    /**
+     * Kinda like wp's autop function
+     * replaces newlines with paragraphs, removes duplicate <br>:s and so on
+     */
+    public function autop($text) {
+
+        // replace <br> with new line
+        $text = str_replace("<br>", "\n", $text);
+
+        // wrap paragraphs around each line
+        $text = '<p>' . preg_replace('/[\r\n]+/', '</p><p>', $text) . '</p>';
+
+        // remove duplicate <br>
+        $text = preg_replace('/(<br>)+/', '<br>', $text);
+
+        return $text;
+
+    }
+
+    public static function getEventsNearLocation($lat, $lng, $nearbyCount = 10, $nearbyInKm = 25) {
+
+        $events = CrimeEvent::selectRaw('*, ( 6371 * acos( cos( radians(?) ) * cos( radians( location_lat ) ) * cos( radians( location_lng ) - radians(?) ) + sin( radians(?) ) * sin( radians( location_lat ) ) ) ) AS distance', [ $lat, $lng, $lat ])
+        ->having("distance", "<=", $nearbyInKm) // välj de som är rimligt nära, värdet är i km
+        ->orderBy("parsed_date", "DESC")
+        ->orderBy("distance", "ASC")
+        ->limit($nearbyCount)
+        ->get();
+
+        return $events;
+
+    }
+
+    public function getViewportSize() {
+
+        $viewportSize = ($this->viewport_northeast_lat - $this->viewport_southwest_lat) + ($this->viewport_northeast_lng - $this->viewport_southwest_lng);
+
+        return $viewportSize;
+
+    }
+
+    /**
+     * > 26 = hela sverige
+     * ca 12 = norrbotten
+     * ca 10 = västerbotten
+     * ca 8 = jämtland
+     * ca 7 = dalarna
+     * ca 6 = västernorrland
+     * ca 1.5 - 2 = län
+     * ca 0.6 = stockholm
+     * ca 0.2 = södertälje
+     * ca 0.1 = typ kungsholmen
+     * ca 0.005 - 0.06 = typ längre gata
+     * mindre än det = jäkla nära
+     */
+    public function getViewPortSizeAsString() {
+
+        $size = $this->getViewportSize();
+       
+        $sizeAsString = "";
+
+        switch ($size) {
+
+            case $size > 20:
+                $sizeAsString = "veryfar";
+                break;
+
+            case $size > 6:
+                $sizeAsString = "far";
+                break;
+        
+             case $size > 0.8:
+                $sizeAsString = "lan";
+                break;
+
+             case $size > 0.1:
+                $sizeAsString = "town";
+                break;
+
+             case $size > 0.05:
+                $sizeAsString = "street";
+                break;
+
+            default:
+                $sizeAsString = "closest";
+
+        }
+
+        return $sizeAsString;
+
+    }
+
+    /**
+     * Return some debug stuff if a query contains some debugActions
+     *
+     * @return array
+     */
+    protected function maybeAddDebugData(\Illuminate\Http\Request $request, CrimeEvent $event) {
+
+        $data = [];
+
+        $debugActions = (array) $request->input("debugActions");
+
+        if (!$debugActions) {
+            return $data;
+        }
+
+        if ( in_array("getLocations", $debugActions) ) {
+
+            // re-get location data from event, i.e find street names again and retun some debug
+            $FeedParserController = new FeedParserController;
+            $FeedController = new FeedController($FeedParserController);
+
+            $itemFoundLocations = $FeedParserController->findLocations($event);
+            $data["itemFoundLocations"] = $itemFoundLocations;
+
+            // get the url that is sent to google to geocode this item
+            $data["itemGeocodeURL"] = $FeedController->getGeocodeURL($event->getKey());
+
+        }
+
+        return $data;
+
+    }
+
+    public function getSingleEventTitle() {
+
+        $title = "";
+        $titleParts = [];
+
+
+        $titleParts[] = $this->parsed_title;
+
+        $prioOneLocations = $this->locations->where("prio", 1);
+
+        foreach ($prioOneLocations as $oneLocation) {
+            $titleParts[] = title_case($oneLocation->name);
+        }
+
+        $titleParts[] = $this->parsed_title_location;
+        $titleParts[] = $this->getPubDateFormatted('%d %b %Y');
+
+        $title = implode(", ", $titleParts);
+
+        return $title;
+
+    }
+
+    public function maybeClearLocationData(\Illuminate\Http\Request $request) {
+
+        $debugActions = (array) $request->input("debugActions");
+
+        if (in_array("clearLocation", $debugActions)) {
+
+            $FeedParserController = new FeedParserController;
+            $FeedController = new FeedController($FeedParserController);
+
+            $this->locations()->delete();
+
+            $this->fill([
+                "scanned_for_locations" => 0,
+                "geocoded" => 0
+            ]);
+
+            $this->save();
+
+            $FeedController->parseItem($this->getKey());
+            $FeedController->geocodeItem($this->getKey());
+
+            $itemFoundLocations = $FeedParserController->findLocations($this);
+            unset($itemFoundLocations[0]["debug"]);
+            $data["itemFoundLocations"] = $itemFoundLocations;
+
+            // get the url that is sent to google to geocode this item
+            $data["itemGeocodeURL"] = $FeedController->getGeocodeURL($this->getKey());
+
+            return $data;
+
+        }
+
+    }
+
+
+}
---
+++ app/Http/Controllers/FeedController.php
@@ -0,0 +1,244 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use Feeds;
+use App\CrimeEvent;
+
+use App\Http\Controllers\FeedParserController;
+
+class FeedController extends Controller
+{
+
+    protected $RssURL = 'https://polisen.se/Stockholms_lan/Aktuellt/Handelser/Handelser-i-hela-landet/?feed=rss';
+    protected $feedParser;
+
+    public function __construct(FeedParserController $feedParser) {
+
+        $this->feedParser = $feedParser;
+
+    }
+
+    public function getGeocodeURL($itemID) {
+
+        $item = CrimeEvent::findOrFail($itemID);
+        $itemLocations = $item->locations;
+        $googleApiKey = getenv('GOOGLE_API_KEY');
+
+        $apiUrlTemplate = 'https://maps.googleapis.com/maps/api/geocode/json?key=' . $googleApiKey . '&language=sv';
+        $apiUrlTemplate .= '&components=country:SE';
+        $apiUrlTemplate .= '&address=%1$s';
+
+        $strLocationURLPart = "";
+
+        foreach ( $itemLocations as $location ) {
+            $strLocationURLPart .= ", " . $location->name;
+        }
+
+        // append main location, from title
+        $strLocationURLPart .= ", " . $item->parsed_title_location;
+        $strLocationURLPart = trim($strLocationURLPart, ", ");
+
+        $strLocationURLPartBeforeUrlEncode = $strLocationURLPart;
+
+        $apiUrl = sprintf(
+            $apiUrlTemplate,
+            urlencode($strLocationURLPart) // 1
+        );
+
+        return $apiUrl;
+
+    }
+
+    public function geocodeItem($itemID) {
+
+        $item = CrimeEvent::findOrFail($itemID);
+        $apiUrl = $this->getGeocodeURL($itemID);
+
+        #echo "\ngeocoding item with title " . $item->title;
+        #echo "\naddress is $strLocationURLPartBeforeUrlEncode";
+        #echo "\napiURL:\n$apiUrl\n";
+
+        $result_data = json_decode( file_get_contents($apiUrl) );
+        $result_status = $result_data->status;
+        $result_results = $result_data->results;
+        #print_r($result_results);
+        // echo "\nstatus code: $result_status";
+        if ($result_results === "OK") {
+            return false;
+        }
+
+        $geometry_location_lat = null;
+        $geometry_location_lng = null;
+
+        foreach ( $result_results as $one_result ) {
+
+            $geometry_location = $one_result->geometry->location;
+            $geometry_location_lat = $geometry_location->lat;
+            $geometry_location_lng = $geometry_location->lng;
+
+            // location_type stores additional data about the specified location.
+            $geometry_type = $one_result->geometry->location_type;
+
+            // viewport contains the recommended viewport for displaying the returned result, specified as two latitude,longitude values defining the southwest and northeast corner of the viewport bounding box. Generally the viewport is used to frame a result when displaying it to a user.
+            $geometry_viewport = $one_result->geometry->viewport;
+
+            $geometry_address_components = $one_result->address_components;
+
+            $administrative_area_level_1 = null;
+            $administrative_area_level_2 = null;
+
+            foreach ($geometry_address_components as $key => $val) {
+                if ( in_array("administrative_area_level_1", $val->types) ) {
+                    $administrative_area_level_1 = $val->long_name;
+                    break;
+                }
+            }
+
+            foreach ($geometry_address_components as $key => $val) {
+                if ( in_array("administrative_area_level_2", $val->types) ) {
+                    $administrative_area_level_2 = $val->long_name;
+                    break;
+                }
+            }
+
+            #echo "\ngeometry_location: " . print_r($geometry_address_components, 1);exit;
+            #echo "\ngeometry_type: " . print_r($geometry_type, 1);
+            #echo "\ngeometry_viewport: " . print_r($geometry_viewport, 1);
+            #exit;
+            #echo "\nlat: $geometry_location_lat";
+            #echo "\nlng: $geometry_location_lng";
+
+            // only return first
+            break;
+
+        }
+        #print_r($result_results);
+
+        if ($geometry_location_lat) {
+            $item->location_lat = $geometry_location_lat;
+            $item->location_lng = $geometry_location_lng;
+            $item->location_geometry_type = $geometry_type;
+            $item->viewport_northeast_lat = $geometry_viewport->northeast->lat;
+            $item->viewport_northeast_lng = $geometry_viewport->northeast->lng;
+            $item->viewport_southwest_lat = $geometry_viewport->southwest->lat;
+            $item->viewport_southwest_lng = $geometry_viewport->southwest->lng;
+            #= json_encode($geometry_viewport, JSON_PRETTY_PRINT);
+            $item->administrative_area_level_1 = $administrative_area_level_1;
+            $item->administrative_area_level_2 = $administrative_area_level_2;
+            $item->geocoded = true;
+            $item->save();
+            #echo "\nadded location for item";
+        }
+
+        #exit;
+
+    }
+
+    /**
+     * Parse an item
+     * Fetches remote info
+     * and finds locations/street names in the text
+     */
+    public function parseItem($itemID) {
+
+        $item = CrimeEvent::findOrFail($itemID);
+
+        // Parse title
+        $parsed_title_items = $this->feedParser->parseTitle($item->title);
+        $item->fill($parsed_title_items);
+        $item->save();
+
+        // Parse permalink, i.e. get info from remote and store
+        $parsed_content_items = $this->feedParser->parseContent($item->permalink);
+        $item->fill($parsed_content_items);
+        $item->save();
+
+        // Find possible locations in teaser and content
+        $locationsByPrio = $this->feedParser->findLocations($item);
+
+        foreach ( $locationsByPrio as $locations) {
+
+            foreach ($locations["locations"] as $locationName) {
+
+                // Add location of not already added
+                #$item->fresh(['locations']);
+                if ( $item->locations->contains("name", $locationName) ) {
+
+                    // echo "\nskipping, location already added $locationName";
+
+                } else {
+
+                    // echo "\nadding location $locationName";
+
+                    $locationModel = new \App\Locations([
+                        "name" => $locationName,
+                        "prio" => $locations["prio"],
+                    ]);
+
+                    $item->locations()->save($locationModel);
+
+                    // we must reload locations so ->contains() will work in the next loop
+                    $item->load('locations');
+                }
+
+            }
+
+        }
+
+        $item->scanned_for_locations = true;
+        $item->save();
+
+    }
+
+    /*
+    Feeds use https://github.com/willvincent/feeds
+    */
+    public function updateFeedsFromPolisen()
+    {
+
+        $feed = \Feeds::make($this->RssURL);
+        $feed_items = $feed->get_items();
+
+        $data = [
+            "numItemsAdded" => 0,
+            "numItemsAlreadyAdded" => 0,
+            "itemsAdded" => []
+        ];
+
+        foreach ($feed_items as $item) {
+
+            $item_md5 = md5($item->get_id());
+
+            $item_data = [
+                "title" => $item->get_title(),
+                "description" => html_entity_decode( $item->get_description() ),
+                "permalink" => $item->get_permalink(),
+                "pubdate" => $item->get_date("U"),
+                "pubdate_iso8601" => $item->get_date(\DateTime::ISO8601),
+                "md5" => $item_md5,
+            ];
+
+            // Store items not already stored
+            $existingItem = CrimeEvent::where("md5", $item_md5)->get();
+
+            if ($existingItem->count()) {
+                $data["numItemsAlreadyAdded"]++;
+                continue;
+            }
+
+            // $data["itemsAdded"][] = $item_data;
+            $data["numItemsAdded"]++;
+
+            $event = CrimeEvent::create($item_data);
+            $data["itemsAdded"][] = $event;
+
+        }
+
+        return $data;
+
+    }
+
+}
