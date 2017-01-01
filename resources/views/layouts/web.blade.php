05de8ba3544c6ea9c552c241b544f254a4e839dc
Pär Thernström <par.thernstrom@gmail.com>
2017-01-01 13:57:09 +0100

    lägg till länkar till händelser per län i sidfot

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -193,13 +193,33 @@ Layout template for web
 
         <ul>
             <li><a href="{{ route("page", ["pagename" => "om"]) }}">Om brotten och kartan</a></li>
+            <li><a href="{{ route("page", ["pagename" => "stockholm"]) }}">Polishändelser i Stockholm</a></li>
             <li><a href="{{ route("page", ["pagename" => "appar"]) }}">Appar till Iphone, Ipad och Android</a></li>
             <li><a href="{{ route("page", ["pagename" => "api"]) }}">Brottsplatser API</a></li>
             <li><a href="https://twitter.com/brottsplatser">Följ @Brottsplatser på Twitter</a></li>
             <li><a href="https://www.facebook.com/brottsplatskartan">Gilla Brottsplatskartan på Facebook</a></li>
             <li><a href="https://stats.uptimerobot.com/ADWQ0TZq1">Upptid/status</a></li>
-            <li><a href="{{ route("page", ["pagename" => "stockholm"]) }}">Polishändelser i Stockholm</a></li>
         </ul>
+
+        <div>
+
+            <p>Händelser per län</p>
+
+            <ul>
+
+                @foreach (App\Helper::getAllLan() as $oneLan)
+
+                    <li class="">
+                        <a href="{{ route("lanSingle", ["lan"=>$oneLan->administrative_area_level_1]) }}">
+                            {{ $oneLan->administrative_area_level_1 }}
+                        </a>
+                    </li>
+
+                @endforeach
+
+            </ul>
+
+        </div>
 
     </footer>
 
