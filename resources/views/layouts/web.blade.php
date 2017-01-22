30b25c486416ece2772d0de8323fc5e9b46041f9
Pär Thernström <par.thernstrom@gmail.com>
2017-01-22 13:13:06 +0100

    add webmanifest

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -74,6 +74,8 @@ Layout template for web
 
     <meta name="apple-itunes-app" content="app-id=1174082309">
 
+    <link rel="manifest" href="/manifest.webmanifest">
+
     <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
     <style amp-custom>{!! file_get_contents( public_path("css/styles.css") ) !!}</style>
 
