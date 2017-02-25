568dca9106c48e2a580f7ac5315b83703fc56fa2
Pär Thernström <par@earthpeople.se>
2017-02-25 22:32:44 +0100

    lägg till följ på twitter och gilla på facebook-texter

--- resources/views/single-event.blade.php
+++ resources/views/single-event.blade.php
@@ -18,6 +18,17 @@ Template for start page
 
     @include('parts.crimeevent', ["single" => true])
 
+    <section>
+        <h2>Följ oss på Twitter</h2>
+        <p>Följ <a href="https://twitter.com/brottsplatser">@brottsplatser</a> för att få alla
+        rapporterade brott i ditt Twitterflöde. Följ <a href="https://twitter.com/StockholmsBrott">@StockholmsBrott</a>
+        för att bara få brott i Stockholms län</p>
+
+        <h2>Gilla oss på Facebook</h2>
+        <p><a href="https://facebook.com/Brottsplatskartan/">facebook.com/Brottsplatskartan</a></p>
+
+    </section>
+
     {{-- show a card with nearby events --}}
     @if (isset($eventsNearby) && $eventsNearby->count())
 
