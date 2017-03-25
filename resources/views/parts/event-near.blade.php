e33ddc490f18db443f1b25e133013b07dae46ff0
Pär Thernström <par.thernstrom@gmail.com>
2017-03-25 21:46:56 +0100

    bättre rubriker på händelser i närheten

--- resources/views/parts/event-near.blade.php
+++ resources/views/parts/event-near.blade.php
@@ -3,18 +3,18 @@
     <h3 class="RelatedEvents__item__title">
         <a class="RelatedEvents__item__link" href="{{ $eventNear->getPermalink() }}">
             {{ $eventNear->parsed_title }},
-            {{ $eventNear->getLocationString(true, true, false) }}
+            {{ $eventNear->getDescriptionAsPlainText() }}
         </a>
     </h3>
 
-    <!-- <p class="RelatedEvents__item__location">{{ $eventNear->getLocationString(true, false, false) }}</p> -->
+    {{-- <p class="RelatedEvents__item__location">{{ $eventNear->getLocationString(true, false, false) }}</p> --}}
 
     {{-- <p class="RelatedEvents__item__date">{{ $eventNear->getParsedDateFormattedForHumans() }}</p> --}}
 
     <p class="RelatedEvents__item__description">
+        {{--{{ $eventNear->getLocationString(true, true, false) }} --}}
         <span class="RelatedEvents__item__date">{{ $eventNear->getParsedDateFormattedForHumans() }}</span>
-        <span class="RelatedEvents__item__dateDivider"> | </span>
-        {{ $eventNear->getDescriptionAsPlainText() }}
+        {{--<span class="RelatedEvents__item__dateDivider"> | </span>--}}
         {{-- {{ $eventNear->getMetaDescription(90) }} --}}
     </p>
 
