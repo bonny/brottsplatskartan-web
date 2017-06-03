4b6d47bf4607864702d506b3cc9646df66508f19
Pär Thernström <par.thernstrom@gmail.com>
2017-06-03 11:38:38 +0200

    gruppera ihop följ oss på twitter och facebook + lite copyfix

--- resources/views/parts/follow-us.blade.php
+++ resources/views/parts/follow-us.blade.php
@@ -1,14 +1,16 @@
 <section>
 
-    <h2>Följ oss på Twitter</h2>
+    <h2>Följ Brottsplatskartan på Twitter och Facebook</h2>
+
     <ul>
+
         <li>Följ <a href="https://twitter.com/brottsplatser">@Brottsplatser</a> för att få alla
     rapporterade brott i ditt Twitterflöde.</li>
-        <li>Följ <a href="https://twitter.com/StockholmsBrott">@StockholmsBrott</a>
-    för att bara få brott i Stockholms län</li>
-    </ul>
 
-    <h2>Gilla oss på Facebook</h2>
-    <p><a href="https://facebook.com/Brottsplatskartan/">facebook.com/Brottsplatskartan</a></p>
+        <li>Följ <a href="https://twitter.com/StockholmsBrott">@StockholmsBrott</a>
+    för att bara få brott i Stockholms län.</li>
+
+        <li>Gilla <a href="https://facebook.com/Brottsplatskartan/">facebook.com/Brottsplatskartan</a> på Facebook för att få nyheter och knasiga brott i ditt flöde.</li>
+    </ul>
 
 </section>
