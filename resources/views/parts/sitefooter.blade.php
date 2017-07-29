f07ef07ebbd2bece0161a2b8ac88a9e18fc1b137
Pär Thernström <par.thernstrom@gmail.com>
2017-07-29 16:43:00 +0200

    lägg till länkar till besöksstatistik hos SIS-index samt snabblänkar till händelser i Sveriges 10 största städer

--- resources/views/parts/sitefooter.blade.php
+++ resources/views/parts/sitefooter.blade.php
@@ -11,6 +11,7 @@
         <li><a href="{{ route("ordlista") }}">Ordlista</a></li>
         <li><a href="{{ route("page", ["pagename" => "api"]) }}">Brottsplatser API</a></li>
         <li><a href="https://stats.uptimerobot.com/ADWQ0TZq1">Upptid/status</a></li>
+        <li><a href="http://www.sis-index.se/site-information/9951" title="Visa antal besökare, besök samt sidvisningar på en fin graf hos SIS-index (Svensk Internet-statistik)">Besöksstatistik</a></li>
     </ul>
 </div>
 
@@ -37,6 +38,24 @@
 </div>
 
 <div class="SiteFooter__col">
+
+    <h2>Händelser i 10 största städerna</h2>
+    <ul class="SiteFooter__navlinks">
+        <li>
+            <a title="Händelser från Polisen i Stockholm" href="{{ route("platsSingle", ['plats' => 'Stockholm']) }}">Stockholm</a>
+            <a title="Händelser från Polisen i Göteborg" href="{{ route("platsSingle", ['plats' => 'Göteborg']) }}">Göteborg</a>
+            <a title="Händelser från Polisen i Malmö" href="{{ route("platsSingle", ['plats' => 'Malmö']) }}">Malmö</a>
+            <a title="Händelser från Polisen i Uppsala" href="{{ route("platsSingle", ['plats' => 'Uppsala']) }}">Uppsala</a>
+            <a title="Händelser från Polisen i Västerås" href="{{ route("platsSingle", ['plats' => 'Västerås']) }}">Västerås</a>
+            <a title="Händelser från Polisen i Örebro" href="{{ route("platsSingle", ['plats' => 'Örebro']) }}">Örebro</a>
+            <a title="Händelser från Polisen i Linköping" href="{{ route("platsSingle", ['plats' => 'Linköping']) }}">Linköping</a>
+            <a title="Händelser från Polisen i Helsingborg" href="{{ route("platsSingle", ['plats' => 'Helsingborg']) }}">Helsingborg</a>
+            <a title="Händelser från Polisen i Jönköping" href="{{ route("platsSingle", ['plats' => 'Jönköping']) }}">Jönköping</a>
+            <a title="Händelser från Polisen i Norrköping" href="{{ route("platsSingle", ['plats' => 'Norrköping']) }}">Norrköping</a>
+        </li>
+    </ul>
+
+
     <h2>Följ oss på Twitter och få senaste Polishändelserna</h2>
     <ul class="SiteFooter__navlinks">
         <li><a href="https://twitter.com/brottsplatser">Följ @Brottsplatser på Twitter</a></li>
