3dc8963a7ce9cb42a059b734f4273f5e40b28455
Pär Thernström <par.thernstrom@gmail.com>
2018-02-04 16:23:06 +0100

    Flytta ut lite län-logik till egen ctronller

--- routes/web.php
+++ routes/web.php
@@ -219,32 +219,11 @@ Route::group(['prefix' => 'admin'], function () {
 
 /**
  * Alla län översikt
+ *
+ * URL är t.ex.:
+ * https://brottsplatskartan.localhost/lan/Stockholms%20l%C3%A4n
  */
-Route::get('/lan/', function (Request $request) {
-    $data = [];
-
-    // some old pages are indexed by google like this
-    // "brottsplatskartan.se/lan?lan=/lan/orebro-lan
-    $old_lan_query = $request->input("lan");
-
-    if ($old_lan_query) {
-        // /lan/orebro-lan
-        $old_lan_query = str_replace('/lan/', '', $old_lan_query);
-        $redirect_to = "lan/{$old_lan_query}";
-        return redirect($redirect_to, 301);
-    }
-
-    $lan = App\Helper::getAllLanWithStats();
-    $data["lan"] = $lan;
-
-    $breadcrumbs = new Creitive\Breadcrumbs\Breadcrumbs;
-    $breadcrumbs->addCrumb('Hem', '/');
-    $breadcrumbs->addCrumb('Län', route("lanOverview"));
-
-    $data["breadcrumbs"] = $breadcrumbs;
-
-    return view('overview-lan', $data);
-})->name("lanOverview");
+Route::get('/lan/', 'lanController@listLan')->name("lanOverview");
 
 /**
  * Alla orter översikt
@@ -649,100 +628,16 @@ Route::get('/ordlista/', function (Request $request) {
 
 /**
  * Ett län, t.ex. Stockholms län
+ *
+ * URL är t.ex.
+ * https://brottsplatskartan.localhost/lan/Stockholms%20l%C3%A4n
+ *
+ * Med paginering är URL t.ex.
+ * https://brottsplatskartan.localhost/lan/Stockholms%20l%C3%A4n?page=2
+ *
  * @param string $lan Namn på län, t.ex. "Stockholms län". Kan även vara "stockholms-län" (med minusstreck)
  */
-Route::get('/lan/{lan}', function ($lan, Request $request) {
-
-    // Om län innehåller minustecken ersätter vi det med mellanslag, pga lagrar länen icke-slug'ade
-    $lan = str_replace('-', ' ', $lan);
-
-    $page = (int) $request->input("page", 1);
-
-    if (!$page) {
-        $page = 1;
-    }
-
-    $events = CrimeEvent::orderBy("created_at", "desc")
-                                ->where("administrative_area_level_1", $lan)
-                                ->with('locations')
-                                ->paginate(10);
-
-    // Hämta mest vanligt förekommande brotten
-    $mostCommonCrimeTypes = CrimeEvent::selectRaw('parsed_title, count(id) as antal')
-        ->where("administrative_area_level_1", $lan)
-        ->groupBy('parsed_title')
-        ->orderByRaw('antal DESC')
-        ->limit(5)
-        ->get();
-
-    $linkRelPrev = null;
-    $linkRelNext = null;
-
-    if ($page > 1) {
-        $linkRelPrev = route('lanSingle', [
-            'lan' => $lan,
-            'page' => $page - 1
-        ]);
-    }
-
-    if ($page < $events->lastpage()) {
-        $linkRelNext = route('lanSingle', [
-            'lan' => $lan,
-            'page' => $page + 1
-        ]);
-    }
-
-    if ($page == 1) {
-        $canonicalLink = route('lanSingle', ['lan' => $lan]);
-    } else {
-        $canonicalLink = route('lanSingle', ['lan' => $lan, 'page' => $page]);
-    }
-
-    $data = [
-        'events' => $events,
-        'lan' => $lan,
-        'page' => $page,
-        'linkRelPrev' => $linkRelPrev,
-        'linkRelNext' => $linkRelNext,
-        'canonicalLink' => $canonicalLink,
-        'mostCommonCrimeTypes' => $mostCommonCrimeTypes
-    ];
-
-    if (!$data["events"]->count()) {
-        abort(404);
-    }
-
-    $breadcrumbs = new Creitive\Breadcrumbs\Breadcrumbs;
-    $breadcrumbs->addCrumb('Hem', '/');
-    $breadcrumbs->addCrumb('Län', route("lanOverview"));
-    $breadcrumbs->addCrumb(e($lan), e($lan));
-
-    $data["breadcrumbs"] = $breadcrumbs;
-    $data["showLanSwitcher"] = true;
-
-    // Kolla om förklarande text för län finns
-    // key = like "introtext-lan-Stockholms län"
-    $introtext_key = "introtext-lan-$lan";
-    $data["introtext"] = Markdown::parse(Setting::get($introtext_key));
-
-    // Hämta statistik för ett län
-    $data["lanChartImgUrl"] = App\Helper::getStatsImageChartUrl($lan);
-
-    $data["lanInfo"] = App\Helper::getSingleLanWithStats($lan);
-
-
-    $mostCommonCrimeTypesMetaDescString = '';
-    foreach ($mostCommonCrimeTypes as $oneCrimeType) {
-        $mostCommonCrimeTypesMetaDescString .= $oneCrimeType->parsed_title . ', ';
-    }
-    $mostCommonCrimeTypesMetaDescString = trim($mostCommonCrimeTypesMetaDescString, ', ');
-
-    $metaDescription = "Se var brott sker i närheten av {$lan}. Vanliga händelser i {$lan} är: {$mostCommonCrimeTypesMetaDescString}. Informationen kommer direkt från Polisen till vår karta.";
-
-    $data['metaDescription'] = $metaDescription;
-
-    return view('single-lan', $data);
-})->name("lanSingle");
+Route::get('/lan/{lan}', 'LanController@lan')->name("lanSingle");
 
 /**
  * Uppdatera saker kring ett single event
