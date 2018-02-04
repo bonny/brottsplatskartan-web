a002ae46f3a50012123235d57130944c4ed07f86
Pär Thernström <par.thernstrom@gmail.com>
2018-02-04 10:46:37 +0100

    Korrigera markup för dagnavigering

--- resources/views/parts/daynav.blade.php
+++ resources/views/parts/daynav.blade.php
@@ -2,11 +2,15 @@
     <nav>
         <ul class="DayNav">
             @if (!empty($prevDayLink))
-                <li class="DayNav__item DayNav__item--prev"><a class="DayNav__link" rel="prev" href="{{ $prevDayLink['link'] }}">{{ $prevDayLink['title'] }}</a></li>
+                <li class="DayNav__item DayNav__item--prev">
+                    <a class="DayNav__link" rel="prev" href="{{ $prevDayLink['link'] }}">{{ $prevDayLink['title'] }}</a>
+                </li>
             @endif
 
             @if (!empty($nextDayLink))
-                <li class="DayNav__item DayNav__item--next"><a <li class="DayNav__link" rel="next" href="{{ $nextDayLink['link'] }}">{{ $nextDayLink['title'] }}</a></li>
+                <li class="DayNav__item DayNav__item--next">
+                    <a class="DayNav__link" rel="next" href="{{ $nextDayLink['link'] }}">{{ $nextDayLink['title'] }}</a>
+                </li>
             @endif
         </ul>
     </nav>
