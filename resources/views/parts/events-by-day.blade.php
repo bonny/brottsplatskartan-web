2de0c27e3df2dfc1c7b07dab6cdd4313426faed7
Pär Thernström <par.thernstrom@gmail.com>
2018-03-23 21:23:09 +0100

    Lägg till göllig datum-ikon vid dag-rubriken

--- resources/views/parts/events-by-day.blade.php
+++ resources/views/parts/events-by-day.blade.php
@@ -1,13 +1,13 @@
 <ul class="Events Events--overview">
     @foreach ($eventsByDay as $dayYmd => $events)
-        <li class="Events--day">
+        <li class="Events__day">
 
-            <h3 class="Events--dayTitle">
+            <h3 class="Events__dayTitle">
                 <time>{{ $events->get(0)->getCreatedAtLocalized() }}</time>
                 <span>– {{ $events->count() }} händelser inhämtade</span>
             </h3>
 
-            <ul class="Events--dayEvents">
+            <ul class="Events__dayEvents">
                 @foreach ($events as $event)
                     @include('parts.crimeevent_v2', ["overview" => true])
                 @endforeach
