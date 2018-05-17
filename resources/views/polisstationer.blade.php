4db28ea78c0d52fd8412b5883428a38b0944a307
Pär Thernström <par.thernstrom@gmail.com>
2018-05-17 20:03:33 +0200

    Piffa upp sidan med polisstationer

--- resources/views/polisstationer.blade.php
+++ resources/views/polisstationer.blade.php
@@ -5,7 +5,6 @@ Template för polisstationer
 
 --}}
 
-
 @extends('layouts.web')
 @section('title', 'Polisstationer')
 
@@ -13,41 +12,56 @@ Template för polisstationer
 
     <h1>Polisstationer</h1>
 
+    <p>
+        Sveriges alla polisstationer i Sverige, grupperat på län.
+    </p>
+
+    {{-- Översikt med ankarsnabblänkar till respektive län --}}
+    <p>Hoppa direkt till län:</p>
     <ul class="PoliceStation-locationsNav">
         @foreach ($locationsByPlace as $place => $placeLocations)
             <li class="PoliceStation-locationsNav-item">
-                <a href="#{{str_slug($place)}}">{{$place}}</a>@if (!$loop->last),@endif
+                <a href="#{{str_slug($place)}}">{{\App\Helper::lanLongNameToShortName($place)}}</a>@if (!$loop->last),@endif
             </li>
         @endforeach
     </ul>
 
-    @foreach ($locationsByPlace as $place => $placeLocations)
+    <div class="PoliceStations-mainListing PoliceStations-mainListing">
 
-        <h2 id="{{str_slug($place)}}">{{$place}}</h2>
+        @foreach ($locationsByPlace as $place => $placeLocations)
 
-        @foreach ($placeLocations as $location)
+            <h2 class="PoliceStations-lanName" id="{{str_slug($place)}}">{{$place}}</h2>
 
-            <h3>{{$location->name}}</h3>
+            <div class="PoliceStations-lanLocations">
+                @foreach ($placeLocations as $station)
 
-            @isset($location->url)
-                {{$location->url}}
-            @endisset
+                    <h3 class="PoliceStation-name">{{$station->name}}</h3>
 
-            {{$location->location->name}}
-            {{-- $location->location->gps --}}
+                    {{-- $station->location->gps --}}
+                    <p class="PoliceStation-street">
+                        <a href="https://www.google.com/maps/search/?api=1&query={{$station->location->gps}}" rel="noopener" target="_blank">
+                            {{$station->location->name}}
+                        </a>
+                    </p>
 
-            @if (is_array($location->services))
-                <p>Tjänster:</p>
-                <ul class="PoliceStation-services">
-                    @foreach ($location->services as $service)
-                        <li class="PoliceStation-service">{{$service->name}}</li>@if (!$loop->last),@endif
-                    @endforeach
-                </ul>
-            @endif
+                    @if (is_array($station->services) && !empty($station->services))
+                        <div class="PoliceStation-services">
+                            <p class="PoliceStation-servicesTitle">Tjänster:</p>
+                            <ul class="PoliceStation-servicesItems">
+                                @foreach ($station->services as $service)
+                                    <li class="PoliceStation-service">
+                                        {{$service->name}}@if (!$loop->last),@endif
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
+
+                @endforeach
+            </div>
 
         @endforeach
-
-    @endforeach
+    </div>
 
     {{--
     @json($location)
