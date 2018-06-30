2deb20f098afb1f29332343281618327d6e88b5d
Pär Thernström <par.thernstrom@gmail.com>
2018-06-30 15:55:15 +0200

    Excludera URLs från CSRF verification så att Facebook kan posta

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends BaseVerifier
      * @var array
      */
     protected $except = [
-        //
+        'https://brottsplatskartan.se',
+        'https://brottsplatskartan.se/'
     ];
 }
