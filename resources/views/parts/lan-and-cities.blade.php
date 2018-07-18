48a1fa96fc156eb018d029accf1a9c8460c40684
Pär Thernström <par.thernstrom@gmail.com>
2018-07-18 12:22:42 +0200

    Rensa i län-widget

--- resources/views/parts/lan-and-cities.blade.php
+++ resources/views/parts/lan-and-cities.blade.php
@@ -2,110 +2,23 @@
 modul i sidebar med länkar till alla län
 --}}
 
-<section class="widget">
+<section class="widget widget--counties">
 
     <h2 class="widget__title">Se senaste händelserna &amp; brotten i ditt län</h2>
 
     <?php
-
-    $arrLan = [
-        [
-            "name" => "Blekinge län",
-            "shortName" => "Blekinge"
-        ],
-        [
-            "name" => "Dalarnas län",
-            "shortName" => "Dalarna"
-        ],
-        [
-            "name" => "Gävleborgs län",
-            "shortName" => "Gävleborg"
-        ],
-        [
-            "name" => "Gotlands län",
-            "shortName" => "Gotland"
-        ],
-        [
-            "name" => "Hallands län",
-            "shortName" => "Halland"
-        ],
-        [
-            "name" => "Jämtlands län",
-            "shortName" => "Jämtland"
-        ],
-        [
-            "name" => "Jönköpings län",
-            "shortName" => "Jönköping"
-        ],
-        [
-            "name" => "Kalmar län",
-            "shortName" => "Kalmar"
-        ],
-        [
-            "name" => "Kronobergs län",
-            "shortName" => "Kronoberg"
-        ],
-        [
-            "name" => "Norrbottens län",
-            "shortName" => "Norrbotten"
-        ],
-        [
-            "name" => "Örebro län",
-            "shortName" => "Örebro"
-        ],
-        [
-            "name" => "Östergötlands län",
-            "shortName" => "Östergötland"
-        ],
-        [
-            "name" => "Skåne län",
-            "shortName" => "Skåne"
-        ],
-        [
-            "name" => "Södermanland and Uppland Södermanlands län",
-            "shortName" => "Södermanland"
-        ],
-        [
-            "name" => "Stockholms län",
-            "shortName" => "Stockholm"
-        ],
-        [
-            "name" => "Uppsala län",
-            "shortName" => "Uppsala"
-        ],
-        [
-            "name" => "Värmlands län",
-            "shortName" => "Värmland"
-        ],
-        [
-            "name" => "Västerbottens län",
-            "shortName" => "Västerbotten"
-        ],
-        [
-            "name" => "Västernorrlands län",
-            "shortName" => "Västernorrland"
-        ],
-        [
-            "name" => "Västmanlands län",
-            "shortName" => "Västmanland"
-        ],
-        [
-            "name" => "Västra Götalands län",
-            "shortName" => "Västa Götaland"
-        ],
-    ];
-
+    $lans = \App\Helper::getLans();
     $lanOutput = "";
 
-    foreach ($arrLan as $lan) {
+    foreach ($lans as $lan) {
 
         $url = route('lanSingle', ['lan' => $lan["name"]]);
 
         $lanOutput .= sprintf(
             '<a href="%3$s">%2$s</a>, ',
-            $lan["name"],
-            $lan["shortName"],
-            $url
+            $lan["name"], // 1
+            $lan["shortName"], // 2
+            $url // 3
         );
     }
 
