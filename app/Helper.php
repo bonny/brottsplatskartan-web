38c771f93c9c90ced7936bea938a594461bd71b0
Pär Thernström <par.thernstrom@gmail.com>
2018-07-18 12:22:18 +0200

    Lägg till helper som hämtar alla län som collection

--- app/Helper.php
+++ app/Helper.php
@@ -508,6 +508,105 @@ class Helper
         return $orter;
     }
 
+    /**
+     * Hämta län i lite vettig collection-format.
+     *
+     * @return collection Collection med län.
+     */
+    public static function getLans()
+    {
+        $lans = [
+            [
+                "name" => "Blekinge län",
+                "shortName" => "Blekinge",
+            ],
+            [
+                "name" => "Dalarnas län",
+                "shortName" => "Dalarna",
+            ],
+            [
+                "name" => "Gävleborgs län",
+                "shortName" => "Gävleborg"
+            ],
+            [
+                "name" => "Gotlands län",
+                "shortName" => "Gotland"
+            ],
+            [
+                "name" => "Hallands län",
+                "shortName" => "Halland"
+            ],
+            [
+                "name" => "Jämtlands län",
+                "shortName" => "Jämtland"
+            ],
+            [
+                "name" => "Jönköpings län",
+                "shortName" => "Jönköping"
+            ],
+            [
+                "name" => "Kalmar län",
+                "shortName" => "Kalmar"
+            ],
+            [
+                "name" => "Kronobergs län",
+                "shortName" => "Kronoberg"
+            ],
+            [
+                "name" => "Norrbottens län",
+                "shortName" => "Norrbotten"
+            ],
+            [
+                "name" => "Örebro län",
+                "shortName" => "Örebro"
+            ],
+            [
+                "name" => "Östergötlands län",
+                "shortName" => "Östergötland"
+            ],
+            [
+                "name" => "Skåne län",
+                "shortName" => "Skåne"
+            ],
+            [
+                "name" => "Södermanland and Uppland Södermanlands län",
+                "shortName" => "Södermanland"
+            ],
+            [
+                "name" => "Stockholms län",
+                "shortName" => "Stockholm"
+            ],
+            [
+                "name" => "Uppsala län",
+                "shortName" => "Uppsala"
+            ],
+            [
+                "name" => "Värmlands län",
+                "shortName" => "Värmland"
+            ],
+            [
+                "name" => "Västerbottens län",
+                "shortName" => "Västerbotten"
+            ],
+            [
+                "name" => "Västernorrlands län",
+                "shortName" => "Västernorrland"
+            ],
+            [
+                "name" => "Västmanlands län",
+                "shortName" => "Västmanland"
+            ],
+            [
+                "name" => "Västra Götalands län",
+                "shortName" => "Västra Götaland"
+            ]
+        ];
+
+        $lans = collect($lans);
+
+        return $lans;
+    }
+
     public static function getLanSlugsToNameArray()
     {
         $arr = [
