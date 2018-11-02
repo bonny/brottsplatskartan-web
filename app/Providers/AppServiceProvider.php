87504050a708c0d39d1c886c22ca7c5924722a5e
Pär Thernström <par.thernstrom@gmail.com>
2018-11-02 22:32:39 +0100

    Dela län med statistik med alla views pga navigation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,13 @@ class AppServiceProvider extends ServiceProvider
     {
         // https://stackoverflow.com/questions/24426423/laravel-generate-secure-https-url-from-route
         \URL::forceScheme('https');
+
+        // Lan listing is used in header nav so share it here for easy access.
+        // https://laravel.com/docs/5.5/views#passing-data-to-views
+        $lan = \App\Helper::getAllLanWithStats();
+        \View::share('lan_with_stats', $lan);
     }
+
 
     /**
      * Register any application services.
