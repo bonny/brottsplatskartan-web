80616138ccc99c1532e7e88b3126d78def50333d
Pär Thernström <par.thernstrom@gmail.com>
2018-11-03 11:46:40 +0100

    Lägg till alla län i sidebar, via accordion

--- resources/views/parts/sidebar.blade.php
+++ resources/views/parts/sidebar.blade.php
@@ -9,11 +9,48 @@ https://www.ampproject.org/docs/reference/components/amp-sidebar
 
         <nav class="Sidebar-nav">
             <ul class="Sidebar-nav-items">
-                <li><a href="{{ route('lanOverview', [], false) }}">Län</a></li>
-                <li><a href="/geo.php">Nära mig</a></li>
-                <li><a href="{{ route('search', [], false) }}">Sök</a></li>
-                <li><a href="{{ route("start") }}">Senaste händelserna</a></li>
+                {{-- <li><a href="{{ route('lanOverview', [], false) }}">Län</a></li> --}}
+
+                <li><a href="{{ route("start") }}">Senaste händelserna i hela Sverige</a></li>
                 <li><a href="{{ route("FullScreenMap") }}">Sverigekartan</a></li>
+                <li><a href="/geo.php">Nära mig</a></li>
+
+
+ <amp-accordion
+                        layout="container"
+                        disable-session-states
+                        class="Sidebar__lan__accordion"
+                        animate
+                        >
+                        <section class="">
+                            <header class="">
+                                Län
+                            </header>
+                            <ul class="Sidebar__lan__accordion__items">
+                                <li class="">
+                                    <a href="{{ route('lanOverview', [], false) }}">
+                                        Översikt alla län
+                                    </a>
+                                </li>
+                                @foreach ($lan_with_stats as $oneLan)
+                                    <li class="">
+                                        <a href="{{ route("lanSingle", ["lan"=>$oneLan->administrative_area_level_1]) }}">
+                                            {{ $oneLan->administrative_area_level_1 }}
+                                        </a>
+
+                                        {{-- <p class="">
+                                            <b>{{ $oneLan->numEvents["today"] }}</b> händelser idag
+                                            <br><b>{{ $oneLan->numEvents["last7days"] }}</b> händelser senaste 7 dagarna
+                                            <br><b>{{ $oneLan->numEvents["last30days"] }}</b> händelser senaste 30 dagarna
+                                        </p> --}}
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </section>
+                    </amp-accordion>
+</li>
+
+                <li><a href="{{ route('search', [], false) }}">Sök</a></li>
                 <li><a href="{{ route("polisstationer") }}">Polisstationer</a></li>
                 <li><a href="{{ route("blog") }}">Blogg</a></li>
                 <li><a href="{{ route("page", ["pagename" => "om"]) }}">Om Brottsplatskartan</a></li>
@@ -26,5 +63,7 @@ https://www.ampproject.org/docs/reference/components/amp-sidebar
             </ul>
         </nav>
     </div>    
+
+
 
 </amp-sidebar>
