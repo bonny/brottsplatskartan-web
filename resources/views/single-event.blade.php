dacbf5c6a0ca1e21c2224f5beb942ab58463b2df
Pär Thernström <par.thernstrom@gmail.com>
2019-03-20 20:17:47 +0100

    Flytta vyn för de mest populära polishändelserna till en egen under-vy

--- resources/views/single-event.blade.php
+++ resources/views/single-event.blade.php
@@ -26,25 +26,8 @@ Template för ett event/händelse
     @if (empty($mostViewed))
         <p>Inget mest tittat på idag ännu.</p>
     @else
-        <section class="widget widget--mostViewed">
-            <h2 class="widget__title">Mest läst</h2>
-            <amp-carousel width="auto" height="175" layout="fixed-height" type="carousel">
-                @foreach ($mostViewed as $view)
-                    <article data-views="{{$view->views}}" class="MostViewed__item">
-                        <h3 class="widget__listItem__title">
-                        <a class="MostViewed__item__link" href="{{ $view->crimeEvent->getPermalink() }}">
-                            <span class="widget__listItem__preTitle">{{$view->crimeEvent->parsed_title}}</span>
-                            <span>{{$view->crimeEvent->getDescriptionAsPlainText()}}</span>
-                        </a>
-                        </h3>
-                        <p class="RelatedEvents__item__location">{{ $view->crimeEvent->getLocationString(false, true, true) }}</p>
-                        <div class="widget__listItem__text">{{ $view->crimeEvent->getParsedContentTeaser(100) }}</div>
-                    </article>
-                @endforeach
-            </amp-carousel>
-        </section>
+        @include('parts.mostViewed')
     @endif
-
 @endsection
 
 @section('sidebar')
