16843444e80e2fd82a72723cf507b9e8b953df6e
Pär Thernström <par.thernstrom@gmail.com>
2019-04-19 21:38:16 +0200

    Lägg till template för brand

---
+++ resources/views/brand.blade.php
@@ -0,0 +1,52 @@
+{{--
+
+Template för /inbrott
+
+--}}
+
+
+@extends('layouts.web')
+
+@section('title', $title)
+
+@section('canonicalLink', $canonicalLink)
+
+@section('content')
+
+    <div class="widget">
+
+        <h1>{{$pageTitle}}</h1>
+
+        @isset($pageSubtitle)
+            <div class="teaser"><p>{{$pageSubtitle}}</p></div>
+        @endisset
+
+        @if ($undersida === 'start')
+            <h2>Brand</h2>
+        @endif
+
+        {{-- <hr /> --}}
+
+        {{-- Gemensamt block längst ner för alla sidor under /inbrott --}}
+
+        <h2>Brandrelaterade händelser från Polisen</h2>
+
+        <p>De senaste händelserna som handlar om t.ex. brand, röklukt, mordbrand, bilbrand
+            rökutveckling.</p>
+
+        <ul class="widget__listItems">
+            @foreach ($latestBrandEvents as $event)
+                @include('parts.crimeevent-small', [
+                    'overview' => true,
+                ])
+            @endforeach
+        </ul>
+    </div>
+
+@endsection
+
+@section('sidebar')
+    @include('parts.widget-blog-entries')
+    @include('parts.lan-and-cities')
+    @include('parts.follow-us')
+@endsection
