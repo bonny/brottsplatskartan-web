4bc11abe96f94344da3fb1d6230461f6e9e92980
Pär Thernström <par.thernstrom@gmail.com>
2019-05-19 21:05:58 +0200

    Cache’a hela vyn för senaste händelserna

--- app/Http/Controllers/MestLastController.php
+++ app/Http/Controllers/MestLastController.php
@@ -11,42 +11,63 @@ use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Cache;
 
 /**
- * Startsidan
+ * Kontroller för sidan med mest lästa händelserna.
+ * https://brottsplatskartan.se/mest-last
  */
 class MestLastController extends Controller
 {
-
     public function index(Request $request)
     {
         $datesBack = 7;
         $numEventsToGet = 10;
-        $days = [];
-        for ($i = 0; $i < $datesBack; $i++) {
-            $date = Carbon::now()->subDays($i);
-            $days[] = [
-                'date' => $date,
-                'title' => sprintf('Mest lästa händelserna %1$s', $date->formatLocalized('%d %B %Y')),
-                'events' => Helper::getMostViewedEvents($date, $numEventsToGet),
-            ];
-        }
 
-        // Make days array into collection.
-        $days = collect($days);
+        $cacheKey = "mestLastaControllerIndexView:V1:D{$datesBack}:N{$numEventsToGet}";
 
-        // Ta bort tomma dagar.
-        $days = $days->reject(function ($value) {
-            return $value['events']->isEmpty();
-        });
+        // Kort cachetid, typ en minut så alltid fräsch lista med senaste händelserna.
+        $cacheTTL = 1 * 60;
 
-        return view(
-            'mestLasta',
-            [
+        $renderedView = Cache::remember($cacheKey, $cacheTTL, function () use (
+            $datesBack,
+            $numEventsToGet
+        ) {
+            $days = [];
+            for ($i = 0; $i < $datesBack; $i++) {
+                $date = Carbon::now()->subDays($i);
+                $days[] = [
+                    'date' => $date,
+                    'title' => sprintf(
+                        'Mest lästa händelserna %1$s',
+                        $date->formatLocalized('%d %B %Y')
+                    ),
+                    'events' => Helper::getMostViewedEvents(
+                        $date,
+                        $numEventsToGet
+                    )
+                ];
+            }
+
+            // Make days array into collection.
+            $days = collect($days);
+
+            // Ta bort tomma dagar.
+            $days = $days->reject(function ($value) {
+                return $value['events']->isEmpty();
+            });
+
+            $view = view('mestLasta', [
                 'mestLastaNyligen' => [
                     'title' => 'Mest lästa nyligen',
-                    'events' => Helper::getMostViewedEventsRecently(20, $numEventsToGet)
+                    'events' => Helper::getMostViewedEventsRecently(
+                        20,
+                        $numEventsToGet
+                    )
                 ],
                 'mestLasta' => $days
-            ]
-        );
+            ]);
+
+            return $view->render();
+        });
+
+        return $renderedView;
     }
 }
