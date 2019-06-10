8e92678e557e18a842ed23ef4fec5952dba6fe3e
Pär Thernström <par.thernstrom@gmail.com>
2019-06-10 18:51:45 +0200

    Lägg till sidebar med direktlänkar till händelser i sveriges 10 störta städer

--- resources/views/parts/lan-and-cities.blade.php
+++ resources/views/parts/lan-and-cities.blade.php
@@ -3,15 +3,13 @@ modul i sidebar med länkar till alla län
 --}}
 
 <section class="widget widget--counties">
-
-    <h2 class="widget__title">Se senaste händelserna &amp; brotten i ditt län</h2>
-
+    <h2 class="widget__title">Senaste händelserna &amp; brotten i ditt län</h2>
     <?php
     $lans = \App\Helper::getLans();
+
     $lanOutput = "";
 
     foreach ($lans as $lan) {
-
         $url = route('lanSingle', ['lan' => $lan["name"]]);
 
         $lanOutput .= sprintf(
@@ -28,7 +26,21 @@ modul i sidebar med länkar till alla län
         '<p>%1$s</p>',
         $lanOutput
     );
-
     ?>
+</section>
 
+<section class="widget widget--cities">
+    <h2 class="widget__title">Senast hänt i din stad</h2>
+    <p>
+        <a title="Händelser från Polisen i Stockholm" href="{{ route("platsSingle", ['plats' => 'Stockholm']) }}">Stockholm</a>
+        <a title="Händelser från Polisen i Göteborg" href="{{ route("platsSingle", ['plats' => 'Göteborg']) }}">Göteborg</a>
+        <a title="Händelser från Polisen i Malmö" href="{{ route("platsSingle", ['plats' => 'Malmö']) }}">Malmö</a>
+        <a title="Händelser från Polisen i Uppsala" href="{{ route("platsSingle", ['plats' => 'Uppsala']) }}">Uppsala</a>
+        <a title="Händelser från Polisen i Västerås" href="{{ route("platsSingle", ['plats' => 'Västerås']) }}">Västerås</a>
+        <a title="Händelser från Polisen i Örebro" href="{{ route("platsSingle", ['plats' => 'Örebro']) }}">Örebro</a>
+        <a title="Händelser från Polisen i Linköping" href="{{ route("platsSingle", ['plats' => 'Linköping']) }}">Linköping</a>
+        <a title="Händelser från Polisen i Helsingborg" href="{{ route("platsSingle", ['plats' => 'Helsingborg']) }}">Helsingborg</a>
+        <a title="Händelser från Polisen i Jönköping" href="{{ route("platsSingle", ['plats' => 'Jönköping']) }}">Jönköping</a>
+        <a title="Händelser från Polisen i Norrköping" href="{{ route("platsSingle", ['plats' => 'Norrköping']) }}">Norrköping</a>
+    </p>
 </section>
