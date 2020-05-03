518e3922856947caf1b765c02e828b64abda5f5a
Pär Thernström <par.thernstrom@gmail.com>
2020-05-03 19:48:21 +0200

    Lägg in datum och lite runda hörn på kartan

--- resources/views/parts/crimeevent-helicopter.blade.php
+++ resources/views/parts/crimeevent-helicopter.blade.php
@@ -1,21 +1,5 @@
 @php
 $eventLink = $event->getPermalink();
-
-// https://stackoverflow.com/questions/23533654/highlighting-all-matched-keyword-in-strings-in-case-insensitive-manner-and-prese
-if (!function_exists("highlightStr")) {
-    function highlightStr($haystack, $needle, $highlightColorValue)
-    {
-        // return $haystack if there is no highlight color or strings given, nothing to do.
-        if (strlen($highlightColorValue) < 1 || strlen($haystack) < 1 || strlen($needle) < 1) {
-            return $haystack;
-        }
-        $haystack = preg_replace("/($needle+)/i", 'yyy<span style="color:'.$highlightColorValue.';">'.'$1'.'</span>', $haystack);
-        return $haystack;
-    }
-}
-
-// echo highlightStr($strMain, $strFind, '#FF0000');
-
 @endphp
 
 <article class="u-margin-top u-padding-top u-border-top">
@@ -29,25 +13,31 @@ if (!function_exists("highlightStr")) {
         </a>
     </h2>
 
-    
-    <div class="">
-        @if ($event->geocoded)
-            <p class="">
-                <a href="{{ $eventLink }}" class="u-block">
-                    <amp-img
-                        layout="responsive"
-                        alt="Karta som visar ungefär var händelsen inträffat"
-                        src="{{ $event->getStaticImageSrc(640,320) }}"
-                        width="640"
-                        height="320">
-                    </amp-img>
-                </a>
-            </p>
-        @endif
-        {{-- {!! $event->getParsedContent() !!} --}}
-        @php
-            echo preg_replace('/(polishelikopter|ambulanshelikopter|helikopter)/i','<em class="highlightedWord">$1</em>', $event->getParsedContent());
-        @endphp
-    </div>
+    <p class="u-color-gray-1 text-sm">
+        <span>
+            <time datetime="{{ $event->getParsedDateISO8601() }}">
+                {{ $event->getParsedDateYMD() }}
+            </time>
+        </span>
+    </p>
+
+    @if ($event->geocoded)
+        <p class="">
+            <a href="{{ $eventLink }}" class="u-block rounded">
+                <amp-img
+                    class="rounded-md"
+                    layout="responsive"
+                    alt="Karta som visar ungefär var händelsen inträffat"
+                    src="{{ $event->getStaticImageSrc(640,320) }}"
+                    width="640"
+                    height="320">
+                </amp-img>
+            </a>
+        </p>
+    @endif
+
+    @php
+    echo preg_replace('/(polishelikopter|ambulanshelikopter|helikopter)/i','<em class="highlightedWord">$1</em>', $event->getParsedContent());
+    @endphp
 
 </article>
