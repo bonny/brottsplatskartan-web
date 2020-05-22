2903d301222f5cd9d1d39cae3b35a00cbb187ee4
Pär Thernström <par.thernstrom@gmail.com>
2020-05-22 22:33:50 +0200

    Forme mest lästa händelserna på startsidan mer likt övriga händelser

--- resources/views/parts/crimeevent-hero.blade.php
+++ resources/views/parts/crimeevent-hero.blade.php
@@ -1,19 +1,29 @@
 <article>
-    @if ($event->geocoded)
-        <p>
-            <a href="{{ $event->getPermalink() }}">
+    <a href="{{ $event->getPermalink() }}" class="u-color-black">
+        @if ($event->geocoded)
+            <p>
                 <amp-img alt="Karta som visar ungefär var händelsen inträffat" class="" src="{{ $event->getStaticImageSrcFar(640,300) }}" width="640" height="300" layout="responsive"></amp-img>
-            </a>
+            </p>
+        @endif
+
+        <p class="u-margin-0 u-margin-bottom-third">
+            <span class="Event__parsedTitle Event__type">{{ $event->parsed_title }}</span>
         </p>
-    @endif
 
-    <h1 class="text-2xl font-bold break-hyphens">
-        <a href="{{ $event->getPermalink() }}">
+        <h1 class="text-2xl font-bold break-hyphens u-margin-0">
             {{ $event->getDescriptionAsPlainText() }}
-        </a>
-    </h1>
+        </h1>
 
-    <a class="" href="{{ $event->getPermalink() }}">
+        <p class="u-color-gray-1 u-margin-top-third u-margin-bottom-third text-sm">
+            <time class="Event__dateHuman__time"
+            title="Tidpunkt då Polisen anger att händelsen inträffat"
+            datetime="{{ $event->getParsedDateISO8601() }}"
+            >
+            {{ $event->getParsedDateFormattedForHumans() }}
+            </time>
+            &middot; {{ $event->getLocationString($includePrioLocations = true, $includeParsedTitleLocation = true, $inclueAdministrativeAreaLevel1Locations = false) }}
+        </p>
+
         <div class="">
             {!! $event->getParsedContentTeaser() !!}
         </div>
