82986123871c0083524328a217528dc843904d0a
Pär Thernström <par.thernstrom@gmail.com>
2020-05-24 11:45:04 +0200

    Gör kartorna på heroes runda

--- resources/views/parts/atoms/event-map-far.blade.php
+++ resources/views/parts/atoms/event-map-far.blade.php
@@ -1,5 +1,12 @@
 @if ($event->geocoded)
     <p class="u-margin-0 u-margin-bottom-half">
-        <amp-img alt="Karta som visar ungefär var händelsen inträffat" class="" src="{{ $event->getStaticImageSrcFar(640,340) }}" width="640" height="340" layout="responsive"></amp-img>
+        <amp-img 
+            src="{{ $event->getStaticImageSrcFar(640,340) }}" 
+            class="rounded" 
+            alt="Karta som visar ungefär var händelsen inträffat" 
+            width="640" 
+            height="340" 
+            layout="responsive"
+        ></amp-img>
     </p>
 @endif
