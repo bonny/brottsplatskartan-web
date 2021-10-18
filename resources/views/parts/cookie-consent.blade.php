0d0fa97f1eb4d2b2ff2b2b68c2b9dba17ddc1052
Pär Thernström <par.thernstrom@gmail.com>
2021-10-18 21:00:14 +0200

    Bättre text i cookies-rutan och lite design på den

--- resources/views/parts/cookie-consent.blade.php
+++ resources/views/parts/cookie-consent.blade.php
@@ -8,18 +8,21 @@
             "xonUpdateHref": "https://example.com/update-consent"
         }
     </script>
-    <div id="consent-ui">
-        <h2 class="site-cookie-banner__messageHeadline">Får vi använda din data till att skräddarsy annonser åt dig?</h2>
-        <p>
-            Vi och våra partners samlar in och använder cookies till annonsanpassning,
-            mätning och för att möjliggöra viktig webbplatsfunktionalitet.
-            <a href="/sida/cookies/" class="site-cookie-banner__readmore">Läs mer om hur TextTV.nu och våra partners
-                samlar in och använder data.</a>
+    <div id="consent-ui" class="consent-ui">
+        <h2 class="consent-ui__headline px-3">Får vi använda din data till att skräddarsy annonser åt dig?</h2>
+        <div class="consent-ui__text px-3">
+            <p class="m-0">
+                Vi och våra partners samlar in och använder cookies till annonsanpassning,
+                mätning och för att möjliggöra viktig webbplatsfunktionalitet.
+                <a href="/sida/cookies/" class="site-cookie-banner__readmore">Läs mer om hur vi &amp; våra partners
+                    samlar in och använder data.</a>
+            </p>
+        </div>
+        <p class="m-0 flex justify-between">
+            <button on="tap:consent-element.accept" class="consent-ui__btnOk">Jag godkänner</button>
+            <button on="tap:consent-element.reject" class="consent-ui__btnNo">Neka</button>
+            {{-- <button on="tap:consent-element.dismiss">Dismiss</button> --}}
         </p>
-
-        <button on="tap:consent-element.accept">Jag godkänner</button>
-        <button on="tap:consent-element.reject">Neka</button>
-        {{-- <button on="tap:consent-element.dismiss">Dismiss</button> --}}
     </div>
 </amp-consent>
 
