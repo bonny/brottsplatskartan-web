f471db202f8009f66a9405a1b7b2c947382d794b
Pär Thernström <par.thernstrom@gmail.com>
2022-03-18 08:42:01 +0100

    Försök lägga till lite sökparametrar

--- resources/views/adsenseSearch.blade.php
+++ resources/views/adsenseSearch.blade.php
@@ -4,12 +4,11 @@
 @section('canonicalLink', $canonicalLink)
 
 @push('scripts')
-    
 @endpush
 
 @section('content')
     <script async src="https://cse.google.com/cse.js?cx=06ceb531c1dfd4f3a"></script>
-    <div class="gcse-search"></div>
+    <div class="gcse-search" enableAutoComplete="true" data-sort_by="date" enableOrderBy></div>
 @endsection
 
 {{-- @section('sidebar')
