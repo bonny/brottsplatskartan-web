33ca45353e57ca60ad2b3680e6729430ee227097
Pär Thernström <par.thernstrom@gmail.com>
2022-03-18 08:14:31 +0100

    Lägg till testsida med Adsense Search

---
+++ resources/views/adsenseSearch.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.web')
+
+@section('title', $pageTitle)
+@section('canonicalLink', $canonicalLink)
+
+@push('scripts')
+    
+@endpush
+
+@section('content')
+    <script async src="https://cse.google.com/cse.js?cx=06ceb531c1dfd4f3a"></script>
+    <div class="gcse-search"></div>
+@endsection
+
+{{-- @section('sidebar')
+    @include('parts.widget-blog-entries')
+    @include('parts.lan-and-cities')
+    @include('parts.follow-us')
+@endsection --}}
