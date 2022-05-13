6d420878bc5c52172a284189d586ea6d85f5917b
Pär Thernström <par.thernstrom@gmail.com>
2022-05-13 15:07:13 +0200

    Använd stevegrunwell/time-constants istället för egna konstanter som bråkade med `artisan optimize`

--- config/constants.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define( 'MINUTE_IN_SECONDS', 60 );
-define( 'HOUR_IN_SECONDS',   60 * MINUTE_IN_SECONDS );
-define( 'DAY_IN_SECONDS',    24 * HOUR_IN_SECONDS   );
-define( 'WEEK_IN_SECONDS',    7 * DAY_IN_SECONDS    );
-define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
