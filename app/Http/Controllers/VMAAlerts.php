d4aba0a3e6f1fa1066ec907ef6b237cef6ba4037
Pär Thernström <par.thernstrom@gmail.com>
2022-05-24 20:55:35 +0200

    Lägg till mer VMA-testdata

--- app/Http/Controllers/VMAAlerts.php
+++ app/Http/Controllers/VMAAlerts.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class VMAAlerts extends Controller
 {
@@ -12,6 +13,51 @@ class VMAAlerts extends Controller
         {
             "timestamp": "2022-05-09T12:34:43+02:00",
             "alerts": [
+                {
+                    "identifier": "SRCAP20220524164316I",
+                    "sender": "https://vmaapi.sr.se/api/v2",
+                    "sent": "2022-05-24T14:43:16+02:00",
+                    "status": "Actual",
+                    "msgType": "Cancel",
+                    "scope": "Public",
+                    "references": "https://vmaapi.sr.se/api/v2,SRCAP20220524111301I,2022-05-24T09:13:01+02:00",
+                    "incidents": "SRVMA20220524111301I",
+                    "info": null
+                },
+                {
+                    "identifier": "SRCAP20220524111301I",
+                    "sender": "https://vmaapi.sr.se/api/v2",
+                    "sent": "2022-05-24T11:13:01+02:00",
+                    "status": "Actual",
+                    "msgType": "Alert",
+                    "scope": "Public",
+                    "references": null,
+                    "incidents": "SRVMA20220524111301I",
+                    "info": [
+                      {
+                        "language": "sv-SE",
+                        "category": "Safety",
+                        "event": "Viktigt meddelande till allmänheten (VMA)",
+                        "urgency": "Immediate",
+                        "severity": "Severe",
+                        "certainty": "Observed",
+                        "senderName": "Sveriges Radio",
+                        "description": "Viktigt meddelande till allmänheten på Hisingen i Göteborgs kommun, Västra Götalands län.\r\n\r\nDet brinner i en industribyggnad på Kongahällavägen, mellan Björlanda och Säve flygplats, med kraftig rökutveckling.\r\n\r\nRäddningsledaren uppmanar alla i området inom en radie av 3 km att gå inomhus och stänga dörrar, fönster och ventilation. \r\n\r\nFör mer information lyssna på Sveriges Radio P4 Göteborg.",
+                        "web": "https://sverigesradio.se/artikel/vma-vad-ar-det",
+                        "area": [
+                          {
+                            "areaDesc": "Göteborgs kommun (Västra Götalands län)",
+                            "geocode": [
+                              {
+                                "valueName": "Kommun",
+                                "value": "1480"
+                              }
+                            ]
+                          }
+                        ]
+                      }
+                    ]
+                  },
                 {
                     "identifier": "SRCAP20210531150110I",
                     "sender": "https://vmaapi.sr.se/api/v2",
@@ -86,5 +132,20 @@ class VMAAlerts extends Controller
 
         $alerts = json_decode($json_data, JSON_PRETTY_PRINT);
         return response()->json($alerts);
+    }
+
+    /**
+     * Importera VMA från SR:s API.
+     * 
+     * @return void 
+     */
+    public static function import() {
+        $url = config('app.vma_alerts_url');
+        $response = Http::get($url);
+        $json = $response->json();
+        $alerts = collect($json['alerts']);
+        $alerts->each(function($alert) {
+            dd($alert);
+        });
     }
 }
