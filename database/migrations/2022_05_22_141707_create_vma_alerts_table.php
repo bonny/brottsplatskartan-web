fee194ee11c96462f2de32f9b3f38ebbb05a2f9c
Pär Thernström <par.thernstrom@gmail.com>
2022-05-24 20:56:00 +0200

    Lägg till migration för att skapa vma-alerts-tabell

---
+++ database/migrations/2022_05_22_141707_create_vma_alerts_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vma_alerts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('identifier');
+            $table->dateTime('sent');
+            $table->string('status');
+            $table->string('msgType');
+            $table->string('references');
+            $table->string('incidents');
+            $table->json('original_message');
+        });
+    }
+
+    /*
+    name	type
+    id	primary key
+    Identifier	string
+    Sent	date
+    status	string
+    msgType	string
+    references	string
+    incidents	string
+    original_message	json
+    */
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vma_alerts');
+    }
+};
