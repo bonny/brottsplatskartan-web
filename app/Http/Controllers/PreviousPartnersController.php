3bf8012c2dc0b0ac329497e6b824a1ad017b0af5
Pär Thernström <par.thernstrom@gmail.com>
2022-05-30 21:34:40 +0200

    Uppdatera prev partners

--- app/Http/Controllers/PreviousPartnersController.php
+++ app/Http/Controllers/PreviousPartnersController.php
@@ -98,22 +98,27 @@ class PreviousPartnersController extends Controller
             [
                 'image' => 'https://brottsplatskartan.ams3.digitaloceanspaces.com/blog/2022-textbild-fyra-nakna-man.png',
                 'link' => 'https://brottsplatskartan.se/blogg/2021/fyra-nakna-man-pa-museeum-och-andra-knasiga-handelser-fran-polisen-2021',
-                'description' => "Fyra nakna män!?"
+                'description' => "Fyra nakna män!?",
             ],
             [
                 'image' => 'https://brottsplatskartan.ams3.digitaloceanspaces.com/blog/2022-textbild-grannsamverkan.png',
-                'link' => 'https://brottsplatskartan.se/inbrott/grannsamverkan',
-                'description' => 'Carehood, Safeland, eller SSF Grannsamverkan. Vilken app väljer du?'
+                'link' => 'https://brottsplatskartan.se/inbrott/grannsamverkan?',
+                'description' => 'Carehood, Safeland, eller SSF Grannsamverkan. Vilken app väljer du?',
             ],
             [
                 'image' => 'https://brottsplatskartan.ams3.digitaloceanspaces.com/blog/2022-textbild-text-tv.png',
-                'link' => 'https://texttv.nu/',
-                'description' => 'Korta, snabba nyheter med Text TV-appen från TextTV.nu'
+                'link' => 'https://texttv.nu/?',
+                'description' => 'Korta, snabba nyheter med Text TV-appen från TextTV.nu',
+            ],
+            [
+                'image' => 'https://brottsplatskartan.ams3.digitaloceanspaces.com/blog/2022-textbild-business-referral-1.jpg',
+                'link' => str_rot13("uggcf://cbeauho.pbz/?erspbqr=uwX-de-12"),
+                'description' => str_rot13('Cbeauho: Serr Cbea Ivqrbf & Frk Zbivrf'),
             ],
         ])->random();       
 
         $link = $randompost['link'];
-        $link = $link . "?utm_source=api-text";
+        $link = $link . "&utm_source=api-text";
 
         $image = $randompost['image'];
         $description = $randompost['description'];
