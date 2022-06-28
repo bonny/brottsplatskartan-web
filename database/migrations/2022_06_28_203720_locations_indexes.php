dabf09590f89e940001c83ce9ce8973e4345a222
Pär Thernström <par.thernstrom@gmail.com>
2022-06-28 20:50:11 +0200

    Mindre name-kolumn pga var för stor för att index skulle skapas + lägg till index pga långsamma frågor

---
+++ database/migrations/2022_06_28_203720_locations_indexes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('locations', function (Blueprint $table) {
+            $table->string('name', 100)->change();
+            $table->index('name');
+            $table->index(['name', 'crime_event_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
