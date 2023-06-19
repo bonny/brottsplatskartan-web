238f6a036dafa8120ddba16e1afb06217a529ca1
Pär Thernström <par.thernstrom@gmail.com>
2023-06-19 09:43:54 +0200

    Flytta upp adminsaker högra upp på event-sidan

--- resources/views/parts/crimeevent.blade.php
+++ resources/views/parts/crimeevent.blade.php
@@ -7,30 +7,27 @@ if $single is set then larger image
 
 --}}
 
-@if(isset($overview))
-
+@if (isset($overview))
     <li
         class="
             Event
-            @if(isset($overview)) Event--overview @endif
-            @if(isset($single)) Event--single @endif
-            @if(isset($event->location_geometry_type)) Event--distance_{{ $event->getViewPortSizeAsString() }} @endif
+            @if (isset($overview)) Event--overview @endif
+            @if (isset($single)) Event--single @endif
+            @if (isset($event->location_geometry_type)) Event--distance_{{ $event->getViewPortSizeAsString() }} @endif
         ">
         <article>
-
-@else
-
-    <article
-        class="
+        @else
+            <article
+                class="
             Event
-            @if(isset($overview)) Event--overview @endif
-            @if(isset($single)) Event--single @endif
-            @if(isset($event->location_geometry_type)) Event--distance_{{ $event->getViewPortSizeAsString() }} @endif
+            @if (isset($overview)) Event--overview @endif
+            @if (isset($single)) Event--single @endif
+            @if (isset($event->location_geometry_type)) Event--distance_{{ $event->getViewPortSizeAsString() }} @endif
         ">
 @endif
 
 
-    {{--
+{{--
 
     "ROOFTOP" indicates that the returned result is a precise geocode for which we have location information accurate down to street address precision.
 
@@ -51,161 +48,134 @@ if $single is set then larger image
     --}}
 
 
-    @if ($event->geocoded)
-        <p class="Event__map">
-            @if ( isset($overview) )
+@if ($event->geocoded)
+    <p class="Event__map">
+        @if (isset($overview))
             <a href="{{ $event->getPermalink() }}">
-            @endif
+        @endif
 
-            @if (isset($overview))
+        @if (isset($overview))
+            <img loading="lazy" alt="{{ $event->getMapAltText() }}" class="Event__mapImage"
+                src="{{ $event->getStaticImageSrc(640, 320) }}" width="640" height="320" layout="responsive"></img>
+        @else
+            <a href="https://google.se/maps/{{ '@' . $event->location_lat }},{{ $event->location_lng }},12z"
+                target="_blank" class="FreddysGoogleMapsLink"
+                title="Öppna ungefärlig plats för händelsen/brottet i Google Maps (öppnas i ny flik)">
+                {{-- @if (false) --}}
+                <span class="Event__mapImageWrap Event__mapImageWrap--near">
+                    <img loading="lazy" alt="{{ $event->getMapAltText() }}"
+                        class="Event__mapImage Event__mapImage--near" src="{{ $event->getStaticImageSrc(617, 463) }}"
+                        width="426" height="320" layout="responsive"></img>
+                </span>
+                {{-- @endif --}}
 
-                <img loading="lazy" alt="{{ $event->getMapAltText() }}" class="Event__mapImage" src="{{ $event->getStaticImageSrc(640,320) }}" width="640" height="320" layout="responsive"></img>
+                <span class="Event__mapImageWrap Event__mapImageWrap--far">
+                    <img loading="lazy"
+                        alt="Översiktskarta som visar hela Sverige med en markör som visar ungefär var händelsen inträffat"
+                        class="Event__mapImage Event__mapImage--far" src="{{ $event->getStaticImageSrcFar(213, 332) }}"
+                        width="213" height="332" layout="responsive"></img>
+                </span>
 
-            @else
-
-                <a
-                    href="https://google.se/maps/{{ '@' . $event->location_lat }},{{ $event->location_lng }},12z"
-                    target="_blank"
-                    class="FreddysGoogleMapsLink"
-                    title="Öppna ungefärlig plats för händelsen/brottet i Google Maps (öppnas i ny flik)"
-                    >
-                    {{-- @if (false) --}}
-                    <span class="Event__mapImageWrap Event__mapImageWrap--near">
-                        <img
-                            loading="lazy"
-                            alt="{{ $event->getMapAltText() }}"
-                            class="Event__mapImage Event__mapImage--near"
-                            src="{{ $event->getStaticImageSrc(617,463) }}"
-                            width="426"
-                            height="320"
-                            layout="responsive"
-                        ></img>
-                    </span>
-                    {{-- @endif --}}
-
-                    <span class="Event__mapImageWrap Event__mapImageWrap--far">
-                        <img
-                            loading="lazy"
-                            alt="Översiktskarta som visar hela Sverige med en markör som visar ungefär var händelsen inträffat"
-                            class="Event__mapImage Event__mapImage--far"
-                            src="{{ $event->getStaticImageSrcFar(213,332) }}"
-                            width="213"
-                            height="332"
-                            layout="responsive"
-                        ></img>
-                    </span>
-
-                </a>
-
-            @endif
-
-            @if ( isset($overview) )
             </a>
-            @endif
-        </p>
-    @endif
+        @endif
 
-    <h1 class="Event__title">
-        @if ( isset($overview) )
+        @if (isset($overview))
+            </a>
+        @endif
+    </p>
+@endif
+
+<h1 class="Event__title">
+    @if (isset($overview))
         <a href="{{ $event->getPermalink() }}">
-        @endif
-            <span class="Event__type">{{ $event->parsed_title }}</span>
-            <span class="Event__teaser">{{ $event->getDescriptionAsPlainText() }}</span>
-        @if ( isset($overview) )
+    @endif
+    <span class="Event__type">{{ $event->parsed_title }}</span>
+    <span class="Event__teaser">{{ $event->getDescriptionAsPlainText() }}</span>
+    @if (isset($overview))
         </a>
-        @endif
-    </h1>
+    @endif
+</h1>
 
-    {{--
+{{--
     Om bara vill visa när skillnad är mer än nn dagar/timmar osv.
     http://stackoverflow.com/questions/23336261/laravel-carbon-display-date-difference-only-in-days
     --}}
-    <p class="Event__meta">
-        @if($event->locations->count())
-            <span class="Event__location">{!! $event->getLocationStringWithLinks() !!}</span>
-        @endif
-        {{-- <span class="Event__metaDivider"> | </span> --}}
-        <span class="Event__dateHuman"><time class="Event__dateHuman__time"
-                  title="Tidpunkt då Polisen anger att händelsen inträffat"
-                  datetime="{{ $event->getParsedDateISO8601() }}"
-                  >{{ $event->getParsedDateFormattedForHumans() }} – {{ $event->getParsedDateYMD() }}
+<p class="Event__meta">
+    @if ($event->locations->count())
+        <span class="Event__location">{!! $event->getLocationStringWithLinks() !!}</span>
+    @endif
+    {{-- <span class="Event__metaDivider"> | </span> --}}
+    <span class="Event__dateHuman"><time class="Event__dateHuman__time"
+            title="Tidpunkt då Polisen anger att händelsen inträffat"
+            datetime="{{ $event->getParsedDateISO8601() }}">{{ $event->getParsedDateFormattedForHumans() }} –
+            {{ $event->getParsedDateYMD() }}
         </time></span>
-    </p>
+</p>
 
-    @if ( isset($overview) )
+@if (isset($overview))
     <a class="Event__contentLink" href="{{ $event->getPermalink() }}">
-    @endif
+@endif
 
-    <div class="Event__content">
-        @if ( isset($overview) )
-            {!! $event->getParsedContentTeaser() !!}
-        @else
-            {!! $event->getParsedContent() !!}
-        @endif
-    </div>
-
-    @if ( isset($overview) )
-    </a>
-    @endif
-
-
-    {{--     <div class="Event__related">
-        Visa fler brott av typ <a href="{{ route("typeSingle", $event->parsed_title ) }}">{{ $event->parsed_title }}</a>
-    </div> --}}
-
-
-    @if(isset($single) && $event->shouldShowSourceLink())
-        <p class="Event__source">Källa: {{ $event->permalink }}</p>
-    @endif
-
-    @if ( isset($overview) )
+<div class="Event__content">
+    @if (isset($overview))
+        {!! $event->getParsedContentTeaser() !!}
     @else
-        @include('parts.crimeevent.newsarticles', ['newsarticles' => $newsarticles])
-
-        @if ($event->isInbrott())
-            <div class="Event__drabbad" id="drabbad_inbrott">
-                <h2 class="Event__drabbad__title">Drabbat av inbrott eller är rädd för att bli?</h2>
-                <p>På vår särskilda sida <a href="{{route('inbrott')}}">om inbrott</a> kan du läsa mer om
-                    hur du gör för att bäst <a href="{{route('inbrott', ['underida' => 'skydda-dig'])}}">skydda dig mot inbrott</a>
-                    och vad du ska göra om du har <a href="{{route('inbrott', ['underida' => 'drabbad'])}}">fått ett inbrott</a>.
-            </div>
-        @endif
-
-        {{-- @if ($event->isBrand())
-            <div class="Event__drabbad" id="drabbad_brand">
-                <h2 class="Event__drabbad__title">Mer om bränder</h2>
-                <p>På vår särskilda sida <a href="{{route('brand')}}">om bränder</a> kan du läsa mer.</p>
-            </div>
-        @endif --}}
-
+        {!! $event->getParsedContent() !!}
     @endif
+</div>
 
-    @if (!empty($dictionaryWordsInText) && $dictionaryWordsInText->count())
-        <aside class="Event__dictionaryWords">
-            <h2 class="Event__dictionaryWordsTitle">Ord som förekommer i händelsen</h2>
+@if (isset($overview))
+    </a>
+@endif
 
-            @foreach ($dictionaryWordsInText as $dictionaryWord)
-                <div class="Event__dictionaryWord">
-                    <h3 class="Event__dictionaryWordTitle">
-                        <a href="{{ route('ordlistaOrd', ['word' => App\Helper::toAscii($dictionaryWord->word)]) }}">
-                            {{ $dictionaryWord->word }}
-                        </a>
-                    </h3>
-                    <div class="Event__dictionaryWordDescription">
-                        <p>{!! str_limit(strip_tags(Markdown::parse($dictionaryWord->description)), 100, '…') !!}</p>
-                    </div>
-                </div>
-            @endforeach
+@if (isset($single) && $event->shouldShowSourceLink())
+    <p class="Event__source">Källa: {{ $event->permalink }}</p>
+@endif
 
-            <p class="Event__dictionaryDictionaryLink"><a href="{{ Route('ordlista') }}">Fler ord hittar du i Ordlistan</a></p>
-
-        </aside>
-    @endif
+@if (isset($overview))
+@else
+    @include('parts.crimeevent.newsarticles', ['newsarticles' => $newsarticles])
 
     @include('parts.admin-crime')
 
-@if(isset($overview))
-        </li>
+    @if ($event->isInbrott())
+        <div class="Event__drabbad" id="drabbad_inbrott">
+            <h2 class="Event__drabbad__title">Drabbat av inbrott eller är rädd för att bli?</h2>
+            <p>På vår särskilda sida <a href="{{ route('inbrott') }}">om inbrott</a> kan du läsa mer om
+                hur du gör för att bäst <a href="{{ route('inbrott', ['underida' => 'skydda-dig']) }}">skydda dig mot
+                    inbrott</a>
+                och vad du ska göra om du har <a href="{{ route('inbrott', ['underida' => 'drabbad']) }}">fått ett
+                    inbrott</a>.
+        </div>
+    @endif
+
+@endif
+
+@if (!empty($dictionaryWordsInText) && $dictionaryWordsInText->count())
+    <aside class="Event__dictionaryWords">
+        <h2 class="Event__dictionaryWordsTitle">Ord som förekommer i händelsen</h2>
+
+        @foreach ($dictionaryWordsInText as $dictionaryWord)
+            <div class="Event__dictionaryWord">
+                <h3 class="Event__dictionaryWordTitle">
+                    <a href="{{ route('ordlistaOrd', ['word' => App\Helper::toAscii($dictionaryWord->word)]) }}">
+                        {{ $dictionaryWord->word }}
+                    </a>
+                </h3>
+                <div class="Event__dictionaryWordDescription">
+                    <p>{!! str_limit(strip_tags(Markdown::parse($dictionaryWord->description)), 100, '…') !!}</p>
+                </div>
+            </div>
+        @endforeach
+
+        <p class="Event__dictionaryDictionaryLink"><a href="{{ Route('ordlista') }}">Fler ord hittar du i Ordlistan</a>
+        </p>
+
+    </aside>
+@endif
+
+@if (isset($overview))
+    </li>
     </article>
 @else
     </article>
