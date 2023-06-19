1249d1a72fb87706e484039609990b552fe7ea4b
Pär Thernström <par.thernstrom@gmail.com>
2023-06-19 09:44:23 +0200

    Lägg händelsen i media-formuläret före locations-formuläret + sätt autofocus på URL när man expanderar

--- resources/views/parts/admin-crime.blade.php
+++ resources/views/parts/admin-crime.blade.php
@@ -1,44 +1,12 @@
 @if (!isset($overview) && Auth::check())
-
     <div class="Event__admin">
 
         <details>
-            <summary>Admingrejjer</summary>
+            <summary class="cursor-pointer"
+                onclick="setTimeout(() => { document.querySelector('input[name=url]').focus(); }, 0);">Admingrejjer
+            </summary>
 
-            <form method='get' action='{{ url()->current() }}' target="_top">
-                <fieldset>
-                    <legend>Platser</legend>
-
-                    <p>
-                        <label>
-                            Lägg till plats<br>
-                            <input type="text" name="locationAdd" placeholder="Hejsanhoppsangränd" autocomplete="off">
-                        </label>
-                    </p>
-
-                    <p>
-                        <label>
-                            Ignorera plats<br>
-                            <input type="text" name="locationIgnore" placeholder="Ipsumvägen" autocomplete="off">
-                        </label>
-                    </p>
-
-                    <p>
-                        <input type="hidden" name="debugActions[]" value="clearLocation">
-                        <button>Rensa location-data &amp; hämta info &amp; plats igen</button>
-                    </p>
-
-                </fieldset>
-            </form>
-
-            <form
-                id="AdminFormAddMediaRef"
-                method='post'
-                class="AdminForm AdminForm--addMediaRef"
-                action-xhr='{{ url()->current() }}'
-                target="_top"
-                on="submit-success: AdminFormAddMediaRef.clear,AdminFormAddMediaRef__successMessage.show"
-            >
+            <form id="AdminFormAddMediaRef" method='post' class="AdminForm AdminForm--addMediaRef" target="_top">
                 <fieldset>
                     <legend>Händelsen i media</legend>
 
@@ -63,25 +31,46 @@
                         <button type="submit">Spara media</button>
                     </p>
 
-                    <div
-                        id="AdminFormAddMediaRef__successMessage"
-                        hidden
-                        >
+                    <div id="AdminFormAddMediaRef__successMessage" hidden>
                         <p>Ok! Tillagd!</p>
                     </div>
 
-                    <div
-                        id="AdminFormAddMediaRef__errorMessage"
-                        hidden
-                        >
+                    <div id="AdminFormAddMediaRef__errorMessage" hidden>
                         <p>Dang, något gick fel när media skulle sparas.</p>
                     </div>
 
                 </fieldset>
 
             </form>
+
+            <form method='get' action='{{ url()->current() }}' target="_top">
+                <fieldset>
+                    <legend>Platser</legend>
+
+                    <p>
+                        <label>
+                            Lägg till plats<br>
+                            <input type="text" name="locationAdd" placeholder="Hejsanhoppsangränd"
+                                autocomplete="off">
+                        </label>
+                    </p>
+
+                    <p>
+                        <label>
+                            Ignorera plats<br>
+                            <input type="text" name="locationIgnore" placeholder="Ipsumvägen" autocomplete="off">
+                        </label>
+                    </p>
+
+                    <p>
+                        <input type="hidden" name="debugActions[]" value="clearLocation">
+                        <button>Rensa location-data &amp; hämta info &amp; plats igen</button>
+                    </p>
+
+                </fieldset>
+            </form>
+
         </details>
 
     </div>
-
 @endif
