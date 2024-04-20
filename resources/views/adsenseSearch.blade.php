5826d881964134cc962b4bb24670698d65e622df
Pär Thernström <par.thernstrom@gmail.com>
2024-04-20 08:46:16 +0200

    Sök: Förbered att skicka sökordsstatistik

--- resources/views/adsenseSearch.blade.php
+++ resources/views/adsenseSearch.blade.php
@@ -12,7 +12,6 @@
 
         <p>Vår databas innehåller över 250.000 händelser.</p>
 
-        
         <script async src="https://cse.google.com/cse.js?cx=06ceb531c1dfd4f3a"></script>
         <div class="gcse-search" enableAutoComplete="true" data-sort_by="date" enableOrderBy></div>
 
@@ -21,6 +20,22 @@
             För att t.ex. hitta skadegörelse i Stockholm så kan du söka efter "skadegörelse stockholm östermalm".
         </p>
     </div>
+
+    <script>
+        // https://developers.google.com/custom-search/docs/element#results-ready
+        function myResultsReadyCallback(gname, query, promoElts, resultElts) {
+            console.log('Användare sökte');
+            console.log('query:', query);
+            console.log('resultElts:', resultElts);
+        }
+
+        window.__gcse || (window.__gcse = {});
+        window.__gcse.searchCallbacks = {
+            web: {
+                ready: myResultsReadyCallback,
+            },
+        };
+    </script>
 @endsection
 
 @section('sidebar')
