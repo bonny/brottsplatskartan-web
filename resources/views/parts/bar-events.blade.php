991a08534cb8374b022ba04fd75b1e99ddb7e30c
Pär Thernström <par.thernstrom@gmail.com>
2024-04-27 12:16:58 +0200

    Lägg till rubrik med länk till händelse-baren

--- resources/views/parts/bar-events.blade.php
+++ resources/views/parts/bar-events.blade.php
@@ -1,11 +1,13 @@
 {{-- Visar en "notification bar" med händelser i en "slider". --}}
 @if ($shared_latest_events->count() > 0)
     <div class="sitebar__Events">
+        <h2 class="sr-only"><a href="{{ route('handelser') }}">Senaste händelserna</a></h2>
         <ul class="sitebar__EventsItems">
             @foreach ($shared_latest_events as $mostViewedItem)
                 <li class="sitebar__EventsItem">
                     <a class="sitebar__EventsItemLink" href="{{ $mostViewedItem->getPermalink() }}">
-                        <span class="sitebar__EventsItem__Time">{{ $mostViewedItem->getParsedDateInFormat('%H:%M') }}</span>
+                        <span
+                            class="sitebar__EventsItem__Time">{{ $mostViewedItem->getParsedDateInFormat('%H:%M') }}</span>
                         {{ $mostViewedItem->title_alt_1 ? $mostViewedItem->title_alt_1 : $mostViewedItem->getSingleEventTitleShort() }}
                     </a>
                 </li>
