1a89fb18558426a80ddd06c3f422f9333bc0b1c5
Pär Thernström <par.thernstrom@gmail.com>
2024-04-27 11:14:55 +0200

    Lägg till order och count till kommando crimeevents:list

--- app/Console/Commands/ListEvents.php
+++ app/Console/Commands/ListEvents.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\CrimeEvent;
+use App\Helper;
+use App\Models\CrimeView;
 use Illuminate\Console\Command;
 
 class ListEvents extends Command {
@@ -11,7 +13,7 @@ class ListEvents extends Command {
      * 
      * @var string
      */
-    protected $signature = 'crimeevents:list';
+    protected $signature = 'crimeevents:list {--order=parsed_date} {--count=10}';
 
     /**
      * The console command description.
@@ -28,7 +30,42 @@ class ListEvents extends Command {
     public function handle() {
         $this->line('Senaste händelserna:');
 
-        $events = CrimeEvent::select(['parsed_date', 'id', 'title'])->limit(10)->orderByDesc('parsed_date')->get();
+        $order = $this->option('order');
+        $count = $this->option('count');
+
+        $valid_orders = ['parsed_date', 'id', 'most_viewed', 'most_viewed_recently'];
+        if (!in_array($order, $valid_orders)) {
+            $this->error('Invalid order option. Valid options are: ' . implode(', ', $valid_orders));
+            return Command::FAILURE;
+        }
+
+        if ( in_array($order, ['parsed_date', 'id']) ) {
+            $events = CrimeEvent::select(['parsed_date', 'id', 'title'])
+            ->orderByDesc($order)
+            ->limit($count)
+            ->get();
+        } elseif ($order === 'most_viewed') {
+            $mostViewed = Helper::getMostViewedEvents(limit: $count);
+            // getMostViewedEventsRecently
+            $events = $mostViewed->map(function (CrimeView $crimeView) {
+                return $crimeView->crimeEvent;
+            });            
+        } elseif ($order === 'most_viewed_recently') {
+            $mostViewed = Helper::getMostViewedEventsRecently(minutes: 25, limit: $count);
+            $events = $mostViewed->map(function (CrimeView $crimeView) {
+                return $crimeView->crimeEvent;
+            });
+        }
+
+        if ( $order === 'most_viewed_recently' || $order === 'most_viewed') {
+            // Hide all attributes or they will be shown in the table.
+            collect($events->first()->getAttributes())->each(function ($value, $key) use ($events) {
+                $events->makeHidden($key);
+            });
+            
+            // Add back just the columns we want to show.
+            $events->makeVisible(['parsed_date', 'id', 'title']);
+        }
 
         $this->table(['Parsed date', 'ID', 'Title'], $events);
         return Command::SUCCESS;
