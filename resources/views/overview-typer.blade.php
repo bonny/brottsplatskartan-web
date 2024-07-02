c11f214da325305f4fc03f5b386e7690e7aecc90
Pär Thernström <par.thernstrom@gmail.com>
2024-07-02 10:41:42 +0200

    Snygga till typ-sidan

--- resources/views/overview-typer.blade.php
+++ resources/views/overview-typer.blade.php
@@ -8,32 +8,27 @@ Template för typer-översikt
 @extends('layouts.web')
 
 @section('title', 'Typer av brott - lista på alla typer av brott som rapporterats')
-@section('metaDescription', e("På Brottsplatskartan kan du se alla typer av brott som rapporterats till Polisen."))
+@section('metaDescription', e('På Brottsplatskartan kan du se alla typer av brott som rapporterats till Polisen.'))
 @section('canonicalLink', '/typ')
 
 @section('content')
 
-    <h1>Senaste brotten sorterat på brottstyp eller händelsetyp</h1>
+    <div class="widget">
+        <h1>Senaste brotten sorterat på brottstyp eller händelsetyp</h1>
 
-    <p>
-        Välj en typ för att se de senaste brotten
-        och händelserna.
-    </p>
+        <p>
+            Välj en typ för att se de senaste brotten
+            och händelserna.
+        </p>
 
-    <p>All data kommer direkt från Polisen.</p>
-
-    <div class="LanListing">
-
-    @foreach ($types as $oneType)
-
-        <h2 class="LanListing__lan">
-            <a href="{{ route("typeSingle", ["typ"=>$oneType->parsed_title]) }}">
-                {{ $oneType->parsed_title }}
-            </a>
-        </h2>
-
-    @endforeach
-
+        <div class="LanListing">
+            @foreach ($types as $oneType)
+                <h2 class="LanListing__lan">
+                    <a href="{{ route('typeSingle', ['typ' => $oneType->parsed_title]) }}">
+                        {{ $oneType->parsed_title }}
+                    </a>
+                </h2>
+            @endforeach
+        </div>
     </div>
-
 @endsection
--- resources/views/single-typ.blade.php
+++ resources/views/single-typ.blade.php
@@ -7,31 +7,23 @@ Template för ett län
 
 @extends('layouts.web')
 
-@section('title', "$type: brott och händelser av typen $type")
-@section('canonicalLink', "/typ/" . mb_strtolower($type))
+@section('title', "$type")
+@section('canonicalLink', '/typ/' . mb_strtolower($type))
 
 @section('content')
 
-    <h1>Brott och händelser av typen {{ $type }}</h1>
+    <div class="widget">
+        <h1>Brott och händelser av typen {{ $type }}</h1>
+        <p>Visar alla inrapporterade händelser och brott för brottstyp {{ $type }}.</p>
 
-    <p>
-        Visar alla inrapporterade händelser och brott för brottstyp {{ $type }}, direkt från polisen.
-    </p>
+        @if ($events)
+            <ul class="Events Events--overview">
+                @foreach ($events as $event)
+                    @include('parts.crimeevent-small', ['overview' => true])
+                @endforeach
+            </ul>
 
-    @if ($events)
-
-        <ul class="Events Events--overview">
-
-            @foreach ($events as $event)
-
-                @include('parts.crimeevent', ["overview" => true])
-
-            @endforeach
-
-        </ul>
-
-        {{ $events->links() }}
-
-    @endif
-
+            {{ $events->links() }}
+        @endif
+    </div>
 @endsection
