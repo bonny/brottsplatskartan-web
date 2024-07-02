4c720e1d8c12afc8ed21b23a8603d2fe835688f1
Pär Thernström <par.thernstrom@gmail.com>
2024-07-02 10:00:29 +0200

    Markera valt huvudnavitem

--- resources/views/parts/siteheader.blade.php
+++ resources/views/parts/siteheader.blade.php
@@ -19,25 +19,59 @@
     <nav class="SiteNav">
         <div class="SiteNav__inner">
             <ul class="SiteNav__items">
-                <li class="SiteNav__item SiteNav__item--latest">
+                <li @class([
+                    'SiteNav__item',
+                    'SiteNav__item--latest',
+                    'is-current' => request()->routeIs('start'),
+                ])>
                     <a href="{{ route('start') }}">Händelser</a>
                 </li>
-                <li class="SiteNav__item SiteNav__item--latest">
+                <li @class([
+                    'SiteNav__item',
+                    'SiteNav__item--latest',
+                    'is-current' => request()->routeIs('handelser'),
+                ])>
                     <a href="{{ route('handelser', [], false) }}">Senaste</a>
                 </li>
-                <li class="SiteNav__item SiteNav__item--mostRead">
+
+                <li @class([
+                    'SiteNav__item',
+                    'SiteNav__item--mostRead',
+                    'is-current' => request()->routeIs('mostRead'),
+                ])>
                     <a href="{{ route('mostRead', [], false) }}">Mest lästa</a>
                 </li>
-                <li class="SiteNav__item SiteNav__item--lan">
+
+
+                <li @class([
+                    'SiteNav__item',
+                    'SiteNav__item--lan',
+                    'is-current' => request()->routeIs('lanOverview'),
+                ])>
                     <a href="{{ route('lanOverview', [], false) }}">Län</a>
                 </li>
-                <li class="SiteNav__item SiteNav__item--geo">
-                    <a href="/nara-hitta-plats">Nära</a>
+
+                <li @class([
+                    'SiteNav__item',
+                    'SiteNav__item--geo',
+                    'is-current' => request()->routeIs('geoDetect'),
+                ])>
+                    <a href="{{ route('geoDetect') }}">Nära</a>
                 </li>
-                <li class="SiteNav__item SiteNav__item--sverigekartan">
+
+                <li @class([
+                    'SiteNav__item',
+                    'SiteNav__item--sverigekartan',
+                    'is-current' => request()->routeIs('sverigekartan'),
+                ])>
                     <a href="{{ route('sverigekartan', [], false) }}">Karta</a>
                 </li>
-                <li class="SiteNav__item SiteNav__item--search">
+
+                <li @class([
+                    'SiteNav__item',
+                    'SiteNav__item--search',
+                    'is-current' => request()->routeIs('adsenseSearch', 'geo'),
+                ])>
                     <a href="{{ route('adsenseSearch', [], false) }}">Sök</a>
                 </li>
             </ul>
