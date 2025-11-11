40fafa2c6c3e7a9bc14f3603777360d35d740293
Pär Thernström <par.thernstrom@gmail.com>
2025-11-11 21:44:47 +0100

    Visa add_cache_time_header som standard

--- config/responsecache.php
+++ config/responsecache.php
@@ -35,7 +35,7 @@ return [
      * should be added to a cached response. This can be handy when
      * debugging.
      */
-    'add_cache_time_header' => env('APP_DEBUG', false),
+    'add_cache_time_header' => env('RESPONSE_CACHE_ADD_TIME_HEADER', true),
 
     /*
      * This setting determines the name of the http header that contains
